b67fa1958fed8a4571bd78715db2f0947b638058
chrisrossi <chris@archimedianco.com>
2009-06-24 21:54:51 +0000

    Prep for release.

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '3.2-dev'
+__version__ = '3.2'
 
 import os
 
