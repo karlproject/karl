6d4b4d1eaa33257e6809a3870ad6b782773a104c
Shane Hathaway <shane@willowrise.com>
2009-07-01 20:25:34 +0000

    Moved the body of the peopleconf script to a new utility module.

--- karl/scripts/peopleconf.py
+++ karl/scripts/peopleconf.py
@@ -18,197 +18,18 @@
 
 Reindex if necessary.
 """
-
 from ConfigParser import ConfigParser
-from karl.models.interfaces import IProfile
-from karl.models.peopledirectory import PeopleCategory
 from karl.models.peopledirectory import PeopleDirectory
-from karl.models.peopledirectory import PeopleReport
-from karl.models.peopledirectory import PeopleReportGroup
-from karl.models.peopledirectory import PeopleSection
-from karl.models.peopledirectory import PeopleSectionColumn
 from karl.scripting import get_default_config
 from karl.scripting import open_root
-from karl.security.policy import NO_INHERIT
-from karl.utils import find_profiles
-from karl.views.peopledirectory import COLUMNS
+from karl.utilities.peopleconf import peopleconf
 from lxml import etree
-from repoze.bfg.security import Allow
-from repoze.bfg.security import Deny
-from repoze.bfg.traversal import model_path
 import optparse
 import os
+import sys
 import transaction
 
-class ParseError(Exception):
-    def __init__(self, msg, elem):
-        self.msg = msg
-        self.sourceline = elem.sourceline
-        docinfo = getattr(elem.getroottree(), 'docinfo', None)
-        if docinfo is not None:
-            self.url = docinfo.URL
-        else:
-            self.url = None
-
-    def __str__(self):
-        res = "%s on line %d" % (self.msg, self.sourceline)
-        if self.url:
-            res += ' of %s' % self.url
-        return res
-
-def id_and_title(elem):
-    id = elem.get('id')
-    if not id:
-        raise ParseError("Missing id attribute", elem)
-    title = elem.get('title', id)
-    return id, title
-
-def set_acl(obj, obj_elem):
-    acl_elem = obj_elem.find('acl')
-    if acl_elem is None:
-        return
-    acl = []
-    for e in acl_elem:
-        if e.tag in ('allow', 'deny'):
-            principal = e.get('principal')
-            if not principal:
-                raise ParseError("Missing principal attribute", e)
-            permission = e.get('permission')
-            if not permission:
-                raise ParseError("Missing permission attribute", e)
-            if e.tag == 'allow':
-                ace = (Allow, principal, permission)
-            else:
-                ace = (Deny, principal, permission)
-        elif e.tag == 'no-inherit':
-            ace = NO_INHERIT
-        else:
-            raise ParseError("Unrecognized element", e)
-        acl.append(ace)
-    obj.__acl__ = acl
-
-def parse_report(people, elem):
-    reportid, title = id_and_title(elem)
-    link_title = elem.get('link-title', title)
-    css_class = elem.get('class', 'general')
-    obj = PeopleReport(title, link_title, css_class)
-    for e in elem.findall('filter'):
-        catid = e.get('category')
-        values = e.get('values', '').split()
-        pc = people.categories.get(catid)
-        if pc is None:
-            raise ParseError("No such category defined", e)
-        if not values:
-            raise ParseError("No category values given", e)
-        for v in values:
-            if v not in pc:
-                raise ParseError("No such category value: %s" % v, e)
-        obj.set_filter(catid, values)
-    columns = None
-    e = elem.find('columns')
-    if e is not None:
-        columns = e.get('ids', '').split()
-    if not columns:
-        raise ParseError("No columns given", elem)
-    for colid in columns:
-        if not colid in COLUMNS:
-            raise ParseError("No such column defined: %s" % colid, e)
-    obj.set_columns(columns)
-    return reportid, obj
-
-def parse_reports(people, parent_elem, reportmap):
-    contents = []
-    for e in parent_elem:
-        if e.tag == 'report-group':
-            obj = PeopleReportGroup(e.get('title'))
-            obj.set_reports(parse_reports(people, e, reportmap))
-            contents.append(obj)
-        elif e.tag == 'report':
-            reportid, obj = parse_report(people, e)
-            if reportid in reportmap:
-                raise ParseError("Non-unique report ID", e)
-            reportmap[reportid] = obj
-            contents.append(obj)
-        else:
-            raise ParseError('Unrecognized element', e)
-    return contents
-
-def parse_section(people, section_elem):
-    columns = []
-    reportmap = {}
-    single = False
-    for e in section_elem:
-        if e.tag == 'report':
-            if columns:
-                raise ParseError(
-                    'Section has both columns and a single report', e)
-            if reportmap:
-                raise ParseError(
-                    'Multiple reports provided for a single report section', e)
-            single = True
-            reportid, obj = parse_report(people, e)
-            reportmap[reportid] = obj
-        elif e.tag == 'column':
-            if single:
-                raise ParseError(
-                    'Section has both columns and a single report', e)
-            obj = PeopleSectionColumn()
-            obj.set_reports(parse_reports(people, e, reportmap))
-            columns.append(obj)
-        elif e.tag == 'acl':
-            pass
-        else:
-            raise ParseError('Unrecognized element', e)
-    return columns, reportmap
-
-def peopleconf(peopledir, tree):
-    peopledir.categories.clear()
-    for cat_elem in tree.findall('categories/category'):
-        catid, title = id_and_title(cat_elem)
-        pc = PeopleCategory(title)
-        peopledir.categories[catid] = pc
-        for value_elem in cat_elem.findall('value'):
-            valueid, title = id_and_title(value_elem)
-            pc[valueid] = title
-
-    for secid in list(peopledir.keys()):
-        del peopledir[secid]
-
-    section_order = []
-    for section_elem in tree.findall('sections/section'):
-        secid, title = id_and_title(section_elem)
-        section_order.append(secid)
-        tab_title = section_elem.get('tab-title', title)
-        section = PeopleSection(title, tab_title)
-        peopledir[secid] = section
-        set_acl(section, section_elem)
-
-        columns, reportmap = parse_section(peopledir, section_elem)
-        section.set_columns(columns)
-        for k, v in reportmap.items():
-            section[k] = v
-
-    peopledir.set_order(section_order)
-
-    need_reindex = peopledir.update_indexes()
-    if need_reindex:
-        reindex(peopledir)
-
-def reindex(peopledir):
-    catalog = peopledir.catalog
-    document_map = catalog.document_map
-    profiles = find_profiles(peopledir)
-    for obj in profiles.values():
-        if IProfile.providedBy(obj):
-            path = model_path(obj)
-            docid = document_map.docid_for_address(path)
-            if not docid:
-                docid = document_map.add(path)
-                catalog.index_doc(docid, obj)
-            else:
-                catalog.reindex_doc(docid, obj)
-
-def main():
+def main(argv=sys.argv, root=None, peopleconf_func=peopleconf):
     parser = optparse.OptionParser(description=__doc__)
     parser.add_option('-C', '--config', dest='config',
         help='Path to configuration file (defaults to etc/karl.ini)',
@@ -216,14 +37,15 @@ def main():
     parser.add_option('--dry-run', dest='dryrun',
         action='store_true', default=False,
         help="Don't actually commit the transaction")
-    options, args = parser.parse_args()
+    options, args = parser.parse_args(argv[1:])
     if args:
         parser.error("Too many parameters: %s" % repr(args))
 
     config = options.config
     if config is None:
         config = get_default_config()
-    root, closer = open_root(config)
+    if root is None:
+        root, closer = open_root(config)
 
     cp = ConfigParser()
     cp.read(config)
@@ -236,7 +58,7 @@ def main():
             del root['people']
         if 'people' not in root:
             root['people'] = PeopleDirectory()
-        peopleconf(root['people'], tree)
+        peopleconf_func(root['people'], tree)
     except:
         transaction.abort()
         raise
---
+++ karl/utilities/tests/test_peopleconf.py
@@ -0,0 +1,555 @@
+# Copyright (C) 2008-2009 Open Society Institute
+#               Thomas Moroz: tmoroz@sorosny.org
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License Version 2 as published
+# by the Free Software Foundation.  You may not use, modify or distribute
+# this program under any other version of the GNU General Public License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+"""Tests of karl.utilities.peopleconf"""
+
+import unittest
+from repoze.bfg import testing
+
+class ParseErrorTests(unittest.TestCase):
+
+    def _getTargetClass(self):
+        from karl.utilities.peopleconf import ParseError
+        return ParseError
+
+    def _makeOne(self, msg, elem):
+        return self._getTargetClass()(msg, elem)
+
+    def test_no_document(self):
+        elem = DummyElement()
+        obj = self._makeOne("test", elem)
+        self.assertEqual(obj.msg, "test")
+        self.assertEqual(obj.sourceline, 10)
+        self.assertEqual(obj.url, None)
+        self.assertEqual(str(obj), 'test on line 10')
+
+    def test_with_document(self):
+        elem = DummyElement(docinfo=DummyDocInfo())
+        obj = self._makeOne("test", elem)
+        self.assertEqual(obj.msg, "test")
+        self.assertEqual(obj.sourceline, 10)
+        self.assertEqual(obj.url, '/tmp/file')
+        self.assertEqual(str(obj), 'test on line 10 of /tmp/file')
+
+
+class IdAndTitleTests(unittest.TestCase):
+
+    def _callFUT(self, elem):
+        from karl.utilities.peopleconf import id_and_title
+        return id_and_title(elem)
+
+    def test_minimal(self):
+        from lxml.etree import Element
+        elem = Element('report', id='r1')
+        id, title = self._callFUT(elem)
+        self.assertEqual(id, 'r1')
+        self.assertEqual(title, 'r1')
+
+    def test_with_title(self):
+        from lxml.etree import Element
+        elem = Element('report', id='r1', title='Report One')
+        id, title = self._callFUT(elem)
+        self.assertEqual(id, 'r1')
+        self.assertEqual(title, 'Report One')
+
+    def test_no_id(self):
+        from lxml.etree import Element
+        elem = Element('report')
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, elem)
+
+
+class SetAclTests(unittest.TestCase):
+
+    def _callFUT(self, obj, elem):
+        from karl.utilities.peopleconf import set_acl
+        return set_acl(obj, elem)
+
+    def test_success(self):
+        from karl.security.policy import NO_INHERIT
+        xml = """
+        <any-object>
+            <acl>
+                <allow principal="alice" permission="view"/>
+                <deny principal="bob" permission="edit"/>
+                <no-inherit/>
+            </acl>
+        </any-object>
+        """
+        elem = parse_xml(xml)
+        obj = testing.DummyModel()
+        self._callFUT(obj, elem)
+        self.assertEqual(obj.__acl__, [
+            ('Allow', 'alice', 'view'),
+            ('Deny', 'bob', 'edit'),
+            NO_INHERIT,
+            ])
+
+    def test_missing_principal(self):
+        xml = """
+        <any-object>
+            <acl>
+                <allow permission="view"/>
+            </acl>
+        </any-object>
+        """
+        elem = parse_xml(xml)
+        obj = testing.DummyModel()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, obj, elem)
+
+    def test_missing_permission(self):
+        xml = """
+        <any-object>
+            <acl>
+                <allow principal="alice"/>
+            </acl>
+        </any-object>
+        """
+        elem = parse_xml(xml)
+        obj = testing.DummyModel()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, obj, elem)
+
+    def test_unrecognized(self):
+        xml = """
+        <any-object>
+            <acl>
+                <xml/>
+            </acl>
+        </any-object>
+        """
+        elem = parse_xml(xml)
+        obj = testing.DummyModel()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, obj, elem)
+
+
+class ParseReportTests(unittest.TestCase):
+
+    def _callFUT(self, peopledir, elem):
+        from karl.utilities.peopleconf import parse_report
+        return parse_report(peopledir, elem)
+
+    def test_minimal(self):
+        xml = """
+        <report id="r1">
+            <columns ids="name"/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        reportid, obj = self._callFUT(peopledir, elem)
+        self.assertEqual(reportid, 'r1')
+        self.assertEqual(obj.title, 'r1')
+        self.assertEqual(obj.link_title, 'r1')
+        self.assertEqual(obj.filters, {})
+        self.assertEqual(obj.columns, ('name',))
+
+    def test_complete(self):
+        xml = """
+        <report id="r1" title="Report One" link-title="One">
+            <filter category="office" values="nyc la"/>
+            <filter category="department" values="toys"/>
+            <columns ids="name email"/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        peopledir.categories['office'] = {'nyc': 'NYC', 'la': 'LA'}
+        peopledir.categories['department'] = {'toys': 'Toys'}
+        reportid, obj = self._callFUT(peopledir, elem)
+        self.assertEqual(reportid, 'r1')
+        self.assertEqual(obj.title, 'Report One')
+        self.assertEqual(obj.link_title, 'One')
+        self.assertEqual(obj.filters, {
+            'department': ('toys',),
+            'office': ('nyc', 'la'),
+            })
+        self.assertEqual(obj.columns, ('name', 'email'))
+
+    def test_no_such_category(self):
+        xml = """
+        <report id="r1">
+            <filter category="office" values="nyc"/>
+            <columns ids="name"/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_no_values(self):
+        xml = """
+        <report id="r1">
+            <filter category="office" values=""/>
+            <columns ids="name"/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        peopledir.categories['office'] = {'nyc': 'NYC'}
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_invalid_category_value(self):
+        xml = """
+        <report id="r1">
+            <filter category="office" values="la"/>
+            <columns ids="name"/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        peopledir.categories['office'] = {'nyc': 'NYC'}
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_no_columns(self):
+        xml = """
+        <report id="r1">
+            <filter category="office" values="nyc"/>
+            <columns ids=""/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        peopledir.categories['office'] = {'nyc': 'NYC'}
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_bad_column(self):
+        xml = """
+        <report id="r1">
+            <filter category="office" values="nyc"/>
+            <columns ids="name zodiac"/>
+        </report>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        peopledir.categories['office'] = {'nyc': 'NYC'}
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+
+class ParseReportsTests(unittest.TestCase):
+
+    def _callFUT(self, peopledir, parent_elem, reportmap):
+        from karl.utilities.peopleconf import parse_reports
+        return parse_reports(peopledir, parent_elem, reportmap)
+
+    def test_no_reports(self):
+        xml = """
+        <report-group>
+        </report-group>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        contents = self._callFUT(peopledir, elem, {})
+        self.assertEqual(contents, [])
+
+    def test_complete(self):
+        xml = """
+        <report-group>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+            <report-group title="Musicians">
+                <report id="r2">
+                    <columns ids="name"/>
+                </report>
+            </report-group>
+        </report-group>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        reportmap = {}
+        contents = self._callFUT(peopledir, elem, reportmap)
+        self.assertEqual(len(contents), 2)
+        self.assertEqual(contents[0].title, 'r1')
+        self.assertEqual(contents[1].title, 'Musicians')
+        self.assertEqual(contents[1].reports[0].title, 'r2')
+        self.assertEqual(len(reportmap), 2)
+        self.assert_('r1' in reportmap)
+        self.assert_('r2' in reportmap)
+
+    def test_non_unique_report(self):
+        xml = """
+        <report-group>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+        </report-group>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem, {})
+
+    def test_unrecognized(self):
+        xml = """
+        <report-group>
+            <xml/>
+        </report-group>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem, {})
+
+
+class ParseSectionTests(unittest.TestCase):
+
+    def _callFUT(self, peopledir, elem):
+        from karl.utilities.peopleconf import parse_section
+        return parse_section(peopledir, elem)
+
+    def test_minimal(self):
+        xml = """
+        <section>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        columns, reportmap = self._callFUT(peopledir, elem)
+        self.assertEqual(columns, [])
+        self.assertEqual(reportmap, {})
+
+    def test_column(self):
+        xml = """
+        <section>
+            <column>
+                <report id="r1">
+                    <columns ids="name"/>
+                </report>
+            </column>
+            <column>
+            </column>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        columns, reportmap = self._callFUT(peopledir, elem)
+        self.assertEqual(len(columns), 2)
+        self.assertEqual(len(columns[0].reports), 1)
+        self.assertEqual(reportmap.keys(), ['r1'])
+
+    def test_single_report(self):
+        xml = """
+        <section>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        columns, reportmap = self._callFUT(peopledir, elem)
+        self.assertEqual(columns, [])
+        self.assertEqual(reportmap.keys(), ['r1'])
+
+    def test_no_mix_columns_and_single_report_1(self):
+        xml = """
+        <section>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+            <column>
+                <report id="r2">
+                    <columns ids="name"/>
+                </report>
+            </column>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_no_mix_columns_and_single_report_2(self):
+        xml = """
+        <section>
+            <column>
+                <report id="r2">
+                    <columns ids="name"/>
+                </report>
+            </column>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_multiple_reports_require_columns(self):
+        xml = """
+        <section>
+            <report id="r1">
+                <columns ids="name"/>
+            </report>
+            <report id="r2">
+                <columns ids="name"/>
+            </report>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+    def test_unrecognized(self):
+        xml = """
+        <section>
+            <xml/>
+        </section>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        from karl.utilities.peopleconf import ParseError
+        self.assertRaises(ParseError, self._callFUT, peopledir, elem)
+
+
+class PeopleConfTests(unittest.TestCase):
+
+    def _callFUT(self, peopledir, elem):
+        from karl.utilities.peopleconf import peopleconf
+        return peopleconf(peopledir, elem)
+
+    def test_all(self):
+        xml = """
+        <peopledirectory>
+            <categories>
+                <category id="offices" title="Offices">
+                    <value id="nyc" title="NYC"/>
+                </category>
+            </categories>
+            <sections>
+                <section id="everyone" title="Everyone" tab-title="All">
+                    <acl>
+                        <allow principal="bob" permission="view"/>
+                    </acl>
+                    <column>
+                        <report-group title="Cities">
+                            <report id="ny" title="NYC Office"
+                                    link-title="NYC">
+                                <filter category="offices" values="nyc"/>
+                                <columns ids="name email"/>
+                            </report>
+                        </report-group>
+                    </column>
+                </section>
+            </sections>
+        </peopledirectory>
+        """
+        elem = parse_xml(xml)
+        peopledir = DummyPeopleDirectory()
+        self._callFUT(peopledir, elem)
+        self.assertEqual(peopledir.order, ['everyone'])
+        self.assertEqual(peopledir.keys(), ['everyone'])
+        self.assertEqual(peopledir['everyone'].__acl__,
+            [('Allow', 'bob', 'view')])
+        self.assertEqual(list(peopledir['everyone'].keys()), ['ny'])
+
+
+class ReindexTests(unittest.TestCase):
+
+    def _callFUT(self, peopledir):
+        from karl.utilities.peopleconf import reindex
+        reindex(peopledir)
+
+    def test_no_profiles(self):
+        from karl.testing import DummyCatalog
+        site = testing.DummyModel()
+        peopledir = DummyPeopleDirectory()
+        peopledir.catalog = DummyCatalog()
+        site['people'] = peopledir
+        profiles = testing.DummyModel()
+        site['profiles'] = profiles
+        self._callFUT(peopledir)
+
+    def test_with_new_profile(self):
+        from karl.testing import DummyCatalog
+        site = testing.DummyModel()
+        peopledir = DummyPeopleDirectory()
+        peopledir.catalog = DummyCatalog()
+        site['people'] = peopledir
+        profiles = testing.DummyModel()
+        site['profiles'] = profiles
+
+        p1 = testing.DummyModel()
+        from karl.models.interfaces import IProfile
+        from zope.interface import directlyProvides
+        directlyProvides(p1, IProfile)
+        profiles['p1'] = p1
+
+        self._callFUT(peopledir)
+        self.assertEquals(peopledir.catalog.document_map.added,
+            [(None, '/profiles/p1')])
+
+    def test_with_existing_profile(self):
+        from karl.testing import DummyCatalog
+        site = testing.DummyModel()
+        peopledir = DummyPeopleDirectory()
+        peopledir.catalog = DummyCatalog()
+        site['people'] = peopledir
+        profiles = testing.DummyModel()
+        site['profiles'] = profiles
+
+        p1 = testing.DummyModel()
+        from karl.models.interfaces import IProfile
+        from zope.interface import directlyProvides
+        directlyProvides(p1, IProfile)
+        profiles['p1'] = p1
+
+        peopledir.catalog.document_map.add('/profiles/p1', 10)
+        self._callFUT(peopledir)
+        self.assertEquals(peopledir.catalog.document_map.added,
+            [(10, '/profiles/p1')])
+
+
+class DummyElement:
+    sourceline = 10
+    def __init__(self, docinfo=None):
+        self.docinfo = docinfo
+    def getroottree(self):
+        return self
+
+class DummyDocInfo:
+    URL = '/tmp/file'
+
+class DummyPeopleDirectory(dict):
+    __parent__ = None
+
+    def __init__(self):
+        self.categories = {}
+
+    def set_order(self, order):
+        self.order = order
+
+    def update_indexes(self):
+        return False
+
+def parse_xml(xml):
+    from lxml.etree import parse
+    from StringIO import StringIO
+    return parse(StringIO(xml)).getroot()
