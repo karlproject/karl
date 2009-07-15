ec16e4a0f05b168711f04207d9d1cde872d6e875
chrisrossi <chris@archimedianco.com>
2009-07-15 15:51:55 +0000

    Port mailin monitor changes to paster template.  (Use a route instead of persistence.)

--- karl/paster_templates/karl_project/src/+package+/+package+/mailin_monitor.py
+++ karl/paster_templates/karl_project/src/+package+/+package+/mailin_monitor.py
@@ -1,4 +1,3 @@
-from persistent import Persistent
 from zope.component import queryUtility
 from repoze.bfg.interfaces import ISettings
 from repoze.bfg.security import Allow
@@ -6,7 +5,12 @@ from repoze.bfg.security import Deny
 from repoze.bfg.security import Everyone
 from repoze.mailin.monitor.models import MailInMonitor
 
-class KarlMailInMonitor(Persistent, MailInMonitor):
+class KarlMailInMonitor(MailInMonitor):
+    """
+    Provide our own model for the root of the mail in monitor graph.  This way
+    we provide an acl for security and our own mechanism for getting at config
+    parameters.
+    """
     _pending_db_path = None
     _maildir_path = None
     __acl__ = [
@@ -14,7 +18,9 @@ class KarlMailInMonitor(Persistent, MailInMonitor):
         (Deny, Everyone, ('view', 'manage'))
     ]
 
-    def __init__(self):
+    def __init__(self, environ):
+        """ Factory for bfg router.
+        """
         # override MailInMonitor.__init__
         pass
 
@@ -31,4 +37,3 @@ class KarlMailInMonitor(Persistent, MailInMonitor):
             settings = queryUtility(ISettings)
             self._maildir_path = settings.maildir_path
         return self._maildir_path
-
