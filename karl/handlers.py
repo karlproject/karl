376d7a99378e41f680538243c1414798101a6713
mcdonc <chrism@plope.com>
2009-08-02 15:19:09 +0000

    Response can be None when there's a broken view.

--- karl/handlers.py
+++ karl/handlers.py
@@ -4,8 +4,9 @@ def add_no_cache_headers(event):
     """ Add no-cache headers if this response doesnt already have a
     Cache-Control header"""
     response = event.response
-    if not has_header(response.headerlist, 'Cache-Control'):
-        response.headerlist.append(
-            ('Cache-Control',
-             'max-age=0, must-revalidate, no-cache, no-store'))
+    if response is not None:
+        if not has_header(response.headerlist, 'Cache-Control'):
+            response.headerlist.append(
+                ('Cache-Control',
+                 'max-age=0, must-revalidate, no-cache, no-store'))
         
