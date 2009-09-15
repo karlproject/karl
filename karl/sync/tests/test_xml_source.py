833582eb7a3216cad633b98489fef7ae9c6dba1f
chrisrossi <chris@archimedianco.com>
2009-09-15 18:48:40 +0000

    Baby step in direction of a SAX based implementation.  Was thinking I could do something clever with SAX and coroutines, but I don't think it's going to work out.  Checking in this little bit of code so it's saved somewhere before I delete it.

--- karl/sync/tests/test_xml_source.py
+++ karl/sync/tests/test_xml_source.py
@@ -1,5 +1,17 @@
 import unittest
 
 class XMLContentSourceTests(unittest.TestCase):
+    def _make_one(self, test_file='test_xml_source_1.xml'):
+        import pkg_resources
+        stream = pkg_resources.resource_stream(__name__, test_file)
+        assert stream is not None
+
+        from karl.sync.xml_source import XMLContentSource
+        return XMLContentSource(stream)
+
     def test_import(self):
         from karl.sync.xml_source import XMLContentSource
+
+    def test_name(self):
+        o = self._make_one()
+        self.assertEqual(o.name, 'why-radio-is-awesome')
