e59584c3003f8bded1d46185c3ef695cd68d0abd
chrisrossi <chris@archimedianco.com>
2009-10-13 19:05:38 +0000

    - Removed out of date paster template.  This has been superceded by KarlSample   project.

--- karl/paster.py
+++
@@ -1,9 +0,0 @@
-from paste.script.templates import Template
-
-from paste.util.template import paste_script_template_renderer
-
-class KARLProjectTemplate(Template):
-    _template_dir = 'paster_templates/karl_project'
-    summary = 'KARL customization project'
-    template_renderer = staticmethod(paste_script_template_renderer)
-
--- karl/paster_templates/karl_project/src/+package+/+package+/evolve/zodb/__init__.py
+++
@@ -1,2 +0,0 @@
-# overall code version for ZODB state
-VERSION = 0 
