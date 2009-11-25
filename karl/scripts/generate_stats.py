0ca1f3aab5bd9ce329b9d90ac763516ba93e8acf
chrisrossi <chris@archimedianco.com>
2009-11-25 18:48:35 +0000

    First stab at generating some stats for Karl usage.

--- karl/scripts/generate_stats.py
+++ karl/scripts/generate_stats.py
@@ -15,52 +15,192 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-""" GenerateStats
-
-
 """
+Generate some statistics about usage.
+"""
+
+from optparse import OptionParser
+
+from csv import DictWriter
+import logging
+import sys
+import transaction
+
+from repoze.folder.interfaces import IFolder
+from karl.models.interfaces import ICommunity
+from karl.models.interfaces import IComment
+from karl.content.interfaces import IWikiPage
+from karl.content.interfaces import IBlogEntry
+from karl.content.interfaces import ICalendarEvent
+from karl.content.interfaces import ICommunityFile
 
 from karl.scripting import get_default_config
 from karl.scripting import open_root
-from optparse import OptionParser
-import os
-import sys
-import transaction
-from karl.models.interfaces import ICommunity
-from repoze.bfg.traversal import find_model
-from karl.utils import find_communities
-from datetime import datetime
-import time
 
-def getCommCount(root):
-    communitycoun = find_communities(root)
-    communitycount = len(communitycoun)
-    return communitycoun
+from karl.utils import find_tags
 
-def main():
-    parser = OptionParser(description=__doc__,
-                          usage='usage: %prog [options] username password')
+log = logging.getLogger(__name__)
+
+count = 0
+
+def walk(startnode, visit, leave):
+    """Visit shallow nodes earlier."""
+    def traverse(node):
+        global count
+        if count >= 1000:
+            return
+
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        count += 1
+        visit(node)
+        if IFolder.providedBy(node):
+            for child in node.values():
+                traverse(child)
+        leave(node)
+
+        if hasattr(node, '_p_deactivate'):
+            # attempt to not run out of memory
+            node._p_deactivate()
+
+    return traverse(startnode)
+
+def is_private(context):
+    return getattr(context, 'security_state', None) == 'private'
+
+def last_activity(community):
+    pass # XXX
+
+class CommunitiesReport(object):
+    filename = 'communities.csv'
+
+    columns = [
+        'community',
+        'id',
+        'is_private',
+        'members',
+        'moderators',
+        'last_activity',
+        'create_date',
+        'wiki_pages',
+        'blog_entries',
+        'blog_comments',
+        'files',
+        'calendar_events',
+        'community_tags',
+        'hits_this_month',
+        'percent_engaged',
+        ]
+
+    def __init__(self):
+        self.rows = []
+        self.row = None
+        self.community = None
+
+    def visit(self, context):
+        if ICommunity.providedBy(context):
+            self.community = context
+            self.row = {
+                'community': context.title,
+                'id': context.__name__,
+                'is_private': is_private(context),
+                'members': len(context.member_names),
+                'moderators': len(context.moderator_names),
+                'last_activity': context.content_modified,
+                'create_date': context.created,
+                'wiki_pages': 0,
+                'blog_entries': 0,
+                'blog_comments': 0,
+                'files': 0,
+                'calendar_events': 0,
+                'community_tags': set(),
+                'hits_this_month': 'Unknown',
+                'percent_engaged': 'Unknown'
+                }
+
+        elif self.community is None:
+            return
+
+        else:
+            last_activity = getattr(context, 'content_modified', None)
+            if (last_activity is not None and
+                last_activity > self.row['last_activity']):
+                self.row['last_activity'] = last_activity
+
+        if IWikiPage.providedBy(context):
+            self.row['wiki_pages'] += 1
+        elif IBlogEntry.providedBy(context):
+            self.row['blog_entries'] += 1
+        elif IComment.providedBy(context):
+            self.row['blog_comments'] += 1
+        elif ICommunityFile.providedBy(context):
+            self.row['files'] += 1
+        elif ICalendarEvent.providedBy(context):
+            self.row['calendar_events'] += 1
+
+        tags = find_tags(context)
+        docid = getattr(context, 'docid', None)
+        if docid is not None:
+            for tag in tags.getTags([docid,]):
+                self.row['community_tags'].add(tag)
+
+    def leave(self, context):
+        if ICommunity.providedBy(context):
+            assert context is self.community
+            self.row['community_tags'] = len(self.row['community_tags'])
+            self.rows.append(self.row)
+            self.row = self.community = None
+
+def gen_stats(root):
+    reports = [
+        CommunitiesReport(),
+        ]
+    def visit(context):
+        for report in reports:
+            report.visit(context)
+
+    def leave(context):
+        for report in reports:
+            report.leave(context)
+
+    walk(root, visit, leave)
+    print ''
+
+    for report in reports:
+        write_report(report)
+
+def write_report(report):
+    f = open(report.filename, 'wb')
+    print >>f, ','.join(report.columns)
+    writer = DictWriter(f, report.columns)
+    writer.writerows(report.rows)
+    f.close()
+
+def main(argv=sys.argv):
+    logging.basicConfig()
+    log.setLevel(logging.INFO)
+
+    parser = OptionParser(
+        description="Generate some statistics about Karl usage",
+        usage="%prog [options]",
+        )
+
     parser.add_option('-C', '--config', dest='config', default=None,
         help="Specify a paster config file. Defaults to $CWD/etc/karl.ini")
-
     options, args = parser.parse_args()
 
+    if len(args):
+        parser.error("Command does not take arguments.")
+
     config = options.config
-    if config is None:
+    if not config:
         config = get_default_config()
     root, closer = open_root(config)
-    thiscount =  len(getCommCount(root))
-    me = sys.argv[0]
-    me = os.path.abspath(me)
-    sandbox = os.path.dirname(os.path.dirname(me))
-    dirname = sandbox + '/var/stats/'
-    if not os.path.isdir(dirname):
-        os.mkdir(dirname)
-    outfile_path = os.path.join(dirname, 'stats.csv')
-    oline = str(datetime.fromtimestamp(time.time()).strftime("%m/%d/%y %H:%M"))+","+str(thiscount)+'\n'
-    f = open(outfile_path, 'a')
-    f.write(oline)
-    f.close()
+
+    try:
+        gen_stats(root)
+    finally:
+        transaction.abort()
 
 if __name__ == '__main__':
     main()
