f7c4c7aa5a4374edf6fc2199c7e974ebf4b94636
chrisrossi <chris@archimedianco.com>
2009-12-15 23:09:01 +0000

    Fixed bug in evolve2 step that updated 'Last Activity' for every community.

--- karl/evolve/zodb/evolve2.py
+++ karl/evolve/zodb/evolve2.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+from zope.component import getSiteManager
+
 from repoze.bfg.traversal import model_path
+from repoze.folder.interfaces import IObjectWillBeAddedEvent
 
 from repoze.lemonade.content import create_content
 
@@ -7,22 +11,38 @@ from karl.content.interfaces import ICalendarLayer
 from karl.content.interfaces import ICalendarCategory
 
 from karl.models.interfaces import ICatalogSearch
+from karl.models.interfaces import IContent
+from karl.models.subscribers import set_created
 
 def evolve(context):
     # add default category and layer to all calendars
-    search = ICatalogSearch(context)
-    default_category_name = ICalendarCategory.getTaggedValue('default_name')
-    default_layer_name = ICalendarLayer.getTaggedValue('default_name')
+    # Prevent 'set_created' event handler from being called since it will,
+    # in turn, set the content_modified attribute of community which is used
+    # as the "Last Activity" in the user interface.  We don't want this tweak
+    # to impact a community's last activity.  This means we need to set created
+    # and modified on the new layers and categories ourselves.
+    registry = getSiteManager()
+    registry.adapters.unsubscribe(
+        (IContent, IObjectWillBeAddedEvent), None, set_created)
+    try:
+        search = ICatalogSearch(context)
+        default_category_name = ICalendarCategory.getTaggedValue('default_name')
+        default_layer_name = ICalendarLayer.getTaggedValue('default_name')
+        now = datetime.now()
 
-    cnt, docids, resolver = search(interfaces=[ICalendar])
-    for docid in docids:
-        calendar = resolver(docid)
-        default_category = create_content(ICalendarCategory, 'Default')
-        if not default_category_name in calendar:
-            calendar[default_category_name] = default_category
-            local_layer = create_content(ICalendarLayer,
-                                         "This Calendar's Events Only", 'blue',
-                                         [model_path(default_category)])
-            if not default_layer_name in calendar:
-                calendar[default_layer_name] = local_layer
-
+        cnt, docids, resolver = search(interfaces=[ICalendar])
+        for docid in docids:
+            calendar = resolver(docid)
+            default_category = create_content(ICalendarCategory, 'Default')
+            default_category.created = default_category.modified = now
+            if not default_category_name in calendar:
+                calendar[default_category_name] = default_category
+                local_layer = create_content(ICalendarLayer,
+                                             "This Calendar's Events Only", 'blue',
+                                             [model_path(default_category)])
+                local_layer.created = local_layer.modified = now
+                if not default_layer_name in calendar:
+                    calendar[default_layer_name] = local_layer
+    finally:
+        registry.adapters.subscribe(
+            (IContent, IObjectWillBeAddedEvent), None, set_created)
