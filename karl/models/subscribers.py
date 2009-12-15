b3434663aa40e1bf227c16c4958624d631408574
chrisrossi <chris@archimedianco.com>
2009-12-15 20:18:38 +0000

    Fix indentation.

--- karl/models/subscribers.py
+++ karl/models/subscribers.py
@@ -10,7 +10,7 @@
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -126,7 +126,7 @@ def _modify_community(obj, when):
     # in a community is changed
     community = find_interface(obj, ICommunity)
     if community is not None:
-         community.content_modified = when
+        community.content_modified = when
 
 def delete_community(obj, event):
     # delete the groups related to the community when a community is
