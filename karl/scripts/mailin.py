5838f3f2333124060ce545e17941418240d99833
chrisrossi <chris@archimedianco.com>
2010-01-28 20:59:15 +0000

    Added option to run draino.  Added option to run in daemon mode.

--- karl/scripts/mailin.py
+++ karl/scripts/mailin.py
@@ -20,14 +20,17 @@
 IMailStore-enabled maildir. The actual maildir must be named 'Maildir'
 within that folder.
 """
+from repoze.mailin.scripts.draino import Draino
+from karl.log import set_subsystem
 from karl.scripting import get_default_config
 from karl.scripting import open_root
+from karl.scripting import run_daemon
 from karl.utilities.mailin import MailinRunner
 import optparse
 import os
 import sys
 
-def main(argv=sys.argv, factory=MailinRunner, root=None):
+def main(argv=sys.argv, factory=MailinRunner, root=None, draino=None):
     parser = optparse.OptionParser(
         description=__doc__,
         usage="%prog [options] maildir_root",
@@ -59,6 +62,17 @@ def main(argv=sys.argv, factory=MailinRunner, root=None):
         help="Name of the utlity (implementing "
                 "karl.utilities.interfaces.IMailinTextScrubber) "
                 "used to scrub text of mail-in content.")
+    parser.add_option('--draino', '-d', dest='run_draino',
+                      action='store_true', default=False,
+                      help="Run draino command before processing mailin.")
+    parser.add_option('--daemon', '-D', dest='daemon',
+                      action='store_true', default=False,
+                      help='Run in daemon mode.')
+    parser.add_option('--interval', '-i', dest='interval', type='int',
+                      default=300,
+                      help='Interval, in seconds, between executions when in '
+                           'daemon mode.')
+
     options, args = parser.parse_args(argv[1:])
 
     if len(args) != 1:
@@ -80,14 +94,39 @@ def main(argv=sys.argv, factory=MailinRunner, root=None):
     if options.pq_file is None:
         options.pq_file = os.path.join(maildir_root, 'pending.db')
 
-    if root is None:
-        config = options.config
-        if config is None:
-            config = get_default_config()
-        root, closer = open_root(config)
+    def run(root=root, draino=draino):
+        if draino is None and options.run_draino:
+            draino_args = ['draino', '-vvv', '-p', '%s/Maildir' % maildir_root,
+                           maildir_root]
+            if options.dry_run:
+                draino_args.insert(1, '--dry-run')
+            draino = Draino(draino_args)
 
-    runner = factory(root, maildir_root, options)
-    runner()
+        if root is None:
+            config = options.config
+            if config is None:
+                config = get_default_config()
+            root, closer = open_root(config)
+
+        set_subsystem('mailin')
+        if options.run_draino:
+            draino.run()
+        runner = factory(root, maildir_root, options)
+        runner()
+
+    if options.daemon:
+        # Go ahead and open the app now to initialize logging, but let it
+        # get closed since we don't want to leave it open while the daemon
+        # process is sleeping.
+        if root is None:
+            config = options.config
+            if config is None:
+                config = get_default_config()
+            open_root(config)
+
+        run_daemon('mailin', run, options.interval)
+    else:
+        run()
 
 if __name__ == '__main__':
     main()
