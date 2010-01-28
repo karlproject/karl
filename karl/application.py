44dd3bde654a69f8ed27e9658e34f2c269746bfd
chrisrossi <chris@archimedianco.com>
2010-01-28 20:57:35 +0000

    Moved log configuration into log module.

--- karl/application.py
+++ karl/application.py
@@ -9,7 +9,7 @@ from repoze.bfg.router import make_app as bfg_make_app
 from repoze.who.plugins.zodb.users import Users
 from repoze.zodbconn.finder import PersistentApplicationFinder
 
-from karl.log import setup_log
+from karl.log import configure_log
 from karl.utils import asbool
 from karl.bootstrap.bootstrap import populate
 from karl.bootstrap.interfaces import IBootstrapper
@@ -22,54 +22,39 @@ def appmaker(root):
     return root['site']
 
 def make_app(global_config, **kw):
+    config = global_config.copy()
+    config.update(kw)
+
     # paster app config callback
-    zodb_uri = global_config.get('zodb_uri')
+    zodb_uri = config.get('zodb_uri')
     if zodb_uri is None:
         raise ValueError('zodb_uri must not be None')
     get_root = PersistentApplicationFinder(zodb_uri, appmaker)
 
     # Coerce a value out of the [app:karl] section in the INI file
-    jquery_dev_mode = kw.get('jquery_dev_mode', False)
-    kw['jquery_dev_mode'] = asbool(jquery_dev_mode)
+    jquery_dev_mode = config.get('jquery_dev_mode', False)
+    config['jquery_dev_mode'] = asbool(jquery_dev_mode)
 
     # Set up logging
-    if 'syslog' in kw:
-        syslog = kw.pop('syslog')
-        if ':' in syslog:
-            host, port = syslog.split(':')
-            port = int(port)
-            address = (host, port)
-        elif '/' in syslog:
-            address = syslog
-        else:
-            host, port = syslog, 514
-
-        debug = kw.get('debug', global_config.get('debug', False))
-        if debug:
-            loglevel = logging.DEBUG
-        else:
-            loglevel = logging.INFO
-
-        app_name = kw.pop('syslog_app_name', 'karl')
-        setup_log(app_name, address, loglevel)
+    configure_log(**config)
 
     # Set up logging admin view (coerce instances to list)
-    if 'syslog_view' in kw:
-        kw['syslog_view_instances'] = kw.get('syslog_view_instances',
+    if 'syslog_view' in config:
+        config['syslog_view_instances'] = config.get('syslog_view_instances',
                                              'karl').split()
 
-    if 'logs_view' in kw:
-        kw['logs_view'] = map(os.path.abspath, kw['logs_view'].split())
+    if 'logs_view' in config:
+        config['logs_view'] = map(os.path.abspath, config['logs_view'].split())
 
     # Make BFG app
-    pkg_name = global_config.get('package', None)
+    pkg_name = config.get('package', None)
     if pkg_name is not None:
         __import__(pkg_name)
         package = sys.modules[pkg_name]
-        app = bfg_make_app(get_root, package, options=kw)
+        app = bfg_make_app(get_root, package, options=config)
     else:
         filename = 'karl.includes:standalone.zcml'
-        app = bfg_make_app(get_root, filename=filename, options=kw)
+        app = bfg_make_app(get_root, filename=filename, options=config)
 
     return app
 
