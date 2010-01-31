81e2db5ded36bc02884db883b59cf77b13d7400d
Balazs Ree <ree@greenfinity.hu>
2010-01-31 12:12:47 +0000

    Add outerHtml to aid debugging on non-debuggable browsers (IE)

--- karl/views/static/karl.js
+++ karl/views/static/karl.js
@@ -2181,5 +2181,31 @@ Karl.themeroller = function() {
     }
 };
 
+// Make debugging more fun by providing outerHtml.
+// Credit for this code: http://ivorycity.com/blog/2009/08/18/3/
+jQuery.fn.extend({
+    outerHtml: function(replacement) {
+        // We just want to replace the entire node and contents with
+        // some new html value
+        if (replacement) {
+            return this.each(function() { $(this).replaceWith(replacement); });
+        }
+
+        /*
+        * Now, clone the node, we want a duplicate so we don't remove
+        * the contents from the DOM. Then append the cloned node to
+        * an anonymous div.
+        * Once you have the anonymous div, you can get the innerHtml,
+        * which includes the original tag.
+        */
+        var tmp_node = $("<div></div>").append($(this).clone());
+        var markup = tmp_node.html();
+
+        // Don't forget to clean up or we will leak memory.
+        tmp_node.remove();
+        return markup;
+    }
+});
+
 })();                   // END CLOSURE Karl
 
