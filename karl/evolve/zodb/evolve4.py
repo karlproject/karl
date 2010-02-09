65612776dcee91265d51313e440366240322bb9e
chrisrossi <chris@archimedianco.com>
2010-02-09 23:03:01 +0000

    Add forgotten evolve step.

---
+++ karl/evolve/zodb/evolve4.py
@@ -0,0 +1,17 @@
+from repoze.bfg.traversal import find_model
+from repoze.lemonade.content import IContent
+from karl.content.interfaces import ICalendarLayer
+from karl.utils import find_catalog
+
+def evolve(context):
+    catalog = find_catalog(context)
+    index = catalog['texts']
+    for docid in index.index._docweight.keys():
+        path = catalog.document_map.address_for_docid(docid)
+        context = find_model(context, path)
+        if (ICalendarLayer.providedBy(context) or
+            not IContent.providedBy(context)):
+            index.unindex_doc(docid)
+
+        if hasattr(context, '_p_deactivate'):
+            context._p_deactivate()
