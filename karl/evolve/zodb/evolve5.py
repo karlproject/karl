314cda6461105e43aaeb0943cdd6c2df638c80f5
chrisrossi <chris@archimedianco.com>
2010-02-12 14:21:43 +0000

    Back out work around.

--- karl/evolve/zodb/evolve5.py
+++ karl/evolve/zodb/evolve5.py
@@ -8,18 +8,12 @@ def evolve(context):
     catalog = find_catalog(context)
     index = catalog['texts']
     for docid in index.index._docweight.keys():
-        try:
-            path = catalog.document_map.address_for_docid(docid)
-            context = find_model(context, path)
-            if (ICalendarLayer.providedBy(context) or
-                ICalendarCategory.providedBy(context) or
-                not IContent.providedBy(context)):
-                index.unindex_doc(docid)
+        path = catalog.document_map.address_for_docid(docid)
+        context = find_model(context, path)
+        if (ICalendarLayer.providedBy(context) or
+            ICalendarCategory.providedBy(context) or
+            not IContent.providedBy(context)):
+            index.unindex_doc(docid)
 
-            if hasattr(context, '_p_deactivate'):
-                context._p_deactivate()
-        except KeyError:
-            # Work around some bad data in the db--some calendar categories
-            # got added with '/' characters in their ids.  Skip them for now
-            # until we can figure out what to do with them.
-            print "Bad path in catalog: ", path
+        if hasattr(context, '_p_deactivate'):
+            context._p_deactivate()
