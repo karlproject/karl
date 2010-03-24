f0d3630208bd5bd7fd46a75fa1eb32aefdc02e1e
Jim P Glenn <jpglenn09@gmail.com>
2010-03-24 21:54:08 +0000

    update to selenium test new all_suite1 for those instances without offices

--- karl/seleniumtests/bin-selenium/FF KarlSample_Suite.bat
+++ karl/seleniumtests/bin-selenium/FF KarlSample_Suite.bat
@@ -4,5 +4,5 @@
 echo " runs test1.html against FF"
 echo "saves results as results_fx_kdi-dev_KarlSample_Suite.html"
 
-java -jar "selenium-server-1.0.1\selenium-server.jar" -htmlSuite "*firefox" "http://kdi-dev.sixfeetup.com:6543/" "../all_suite.html" "../log/results_fx_kdi-dev_KarlSample_Suite.html"
+java -jar "selenium-server-1.0.1\selenium-server.jar" -htmlSuite "*firefox" "http://kdi-dev.sixfeetup.com:6543/" "../all_suite1.html" "../log/results_fx_kdi-dev_KarlSample_Suite.html"
 
