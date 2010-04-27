928365196bfd7a0beafa50f1b177faedb425535f
Jim P Glenn <jpglenn09@gmail.com>
2010-04-27 21:02:56 +0000

    updated new staging suite

--- karl/seleniumtests/bin-selenium/FF Fahamu_Staging_Suite.bat
+++ karl/seleniumtests/bin-selenium/FF Fahamu_Staging_Suite.bat
@@ -4,5 +4,5 @@
 echo " runs test1.html against FF"
 echo "saves results as results_fx_Karlhost01_Fahamu_Suite.html"
 
-java -jar "selenium-server-1.0.1\selenium-server.jar" -htmlSuite "*firefox" "http://karlhost01.sixfeetup.com:8200/" "../Fahamu_Suite.html" "../log/results_fx_Karlhost01_Fahamu_Suite.html"
+java -jar "selenium-server-1.0.1\selenium-server.jar" -htmlSuite "*firefox" "http://karlhost01.sixfeetup.com:8200/" "../all_suite.html" "../log/results_fx_Karlhost01_Fahamu_Suite.html"
 
--- karl/seleniumtests/bin-selenium/FF OSI_Staging_Suite.bat
+++ karl/seleniumtests/bin-selenium/FF OSI_Staging_Suite.bat
@@ -1,8 +1,8 @@
 ' runs test1.html against FF
-'saves results as results_fx_karlhost01_OSI_all_suite.html
+'saves results as results_fx_karlhost01_OSI_suite.html
 
 echo " runs test1.html against FF"
-echo "saves results as results_fx_karlhost01_OSI_all_suite.html"
+echo "saves results as results_fx_karlhost01_OSI_suite.html"
 
 java -jar "selenium-server-1.0.1\selenium-server.jar" -htmlSuite "*firefox" "http://karlhost01.sixfeetup.com:8300/" "../OSI_suite.html" "../log/results_fx_karlhost01_OSI_Suite.html"
 
