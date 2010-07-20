a21da788b2ea287faabdf0cadee787b73c78c6d9
Balazs Ree <ree@greenfinity.hu>
2010-07-20 19:11:11 +0000

    Add datetimepicker testsuite

---
+++ karl/views/static/jquery-ui/karl-datetimepicker/demo_test.js
@@ -0,0 +1,18 @@
+
+(function($){
+
+module("karl-datetimepicker");
+
+test("Create and destroy", function() {
+
+
+    $('#datetime1').karldatetimepicker({
+        });
+
+    $('#datetime1').karldatetimepicker('destroy');
+
+});
+
+
+})(jQuery);
+
---
+++ karl/views/static/jquery-ui/karl-datetimepicker/karl.datetimepicker.js
@@ -0,0 +1,8 @@
+
+(function($){
+
+// XXX This code is currently in karl.js
+
+
+})(jQuery);
+
