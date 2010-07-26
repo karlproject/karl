226dcebff3396adbe980235ad8a78d816296362a
chrisrossi <chris@archimedianco.com>
2010-07-26 18:52:06 +0000

    Removed a redundant copy of the text_scrubber code used by mailin.

--- karl/utilities/mailin.py
+++ karl/utilities/mailin.py
@@ -176,32 +176,6 @@ class MailinRunner: # pragma NO COVERAGE (deprecated)
             transaction.commit()
             self.pending.sql.commit()
 
-REPLY_SEPARATOR = re.compile("--- Reply ABOVE THIS LINE to post a comment ---")
-_OUTLOOK = re.compile(r'[>|\s]*-----\s?Original Message\s?-----')
-_OUTLOOK_EXPRESS = re.compile(r'________________________________')
-_GMAIL = re.compile(r'\nOn (Mon|Tue|Wed|Thu|Fri|Sat|Sun), (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Nov|Oct|Dec) \d{1,2}')
-_THUNDERBIRD = re.compile(r'\n[^\n]*wrote:\n>')
-
-cutoffs = [
-    REPLY_SEPARATOR,
-    _OUTLOOK,
-    _OUTLOOK_EXPRESS,
-    _GMAIL,
-    _THUNDERBIRD
-]
-
-def text_scrubber(text, mimetype=None):
-    # We're assuming plain text
-    if mimetype is not None and mimetype != "text/plain":
-        raise Exception("Unsupported mime type: %s" % mimetype)
-
-    for pattern in cutoffs:
-        match = pattern.search(text)
-        if match:
-            text = text[:match.start()].strip()
-
-    return markdown2.markdown(text)
-
 class MailinRunner2(object):
     """
     Compatible with repoze.postoffice.
@@ -213,7 +187,7 @@ class MailinRunner2(object):
         self.queue, self.closer = factory(zodb_uri, queue_name, zodb_path)
         dispatcher = IMailinDispatcher(self.root)
         dispatcher.default_tool = 'blog'
-        dispatcher.text_scrubber = 'karl.utilities.mailin.text_scrubber'
+        dispatcher.text_scrubber = 'karl.utilities.textscrub:text_scrubber'
         self.dispatcher = dispatcher
 
     def __call__(self):
