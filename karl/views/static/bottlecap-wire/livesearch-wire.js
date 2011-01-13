4f9a73e86b098159ceeca37237ab930fd4010c3c
rmarianski <rob@marianski.com>
2011-01-13 20:22:22 +0000

    reintegrate updated js (rendering is not working properly, but should ask server for results now)

--- karl/views/static/bottlecap-wire/livesearch-wire.js
+++ karl/views/static/bottlecap-wire/livesearch-wire.js
@@ -1,23 +1,20 @@
-(function($){
+(function($) {
 
-
-function createUrlFn(urlPrefix) {
+function createUrlFn(urlPrefix, kind) {
     return function(query) {
-        return urlPrefix + '?q=' + escape(query);
+        return kind
+            ? urlPrefix + '?kind=' + escape(kind)
+                        + '&val='  + escape(query)
+            : urlPrefix + '?val='  + escape(query);
     }
 }
 
-
-$(document).ready(function() {
+$(function() {
 
     var app_url = $("#karl-app-url").eq(0).attr('content');
-    ///var livesearch_url = app_url + '/livesearch.json');
-    ///var livesearch_url = app_url + '/livesearch_people.json');
-    // XXX static testing
-    var livesearch_url = app_url + '/static/bottlecap/bc.livesearch/data.json';
-    var livesearch_people_url = app_url + '/static/bottlecap/bc.livesearch/data-people.json';
+    var livesearch_url = app_url + "/jquery_livesearch";
 
-    $('.ui-ls-autocomplete').livesearch({
+    $('.bc-livesearch').livesearch({
         urlFn: createUrlFn(livesearch_url),
         search: function(event, ui) {
             $('<p>Search for ' + ui.query + '</p>')
@@ -25,35 +22,17 @@ $(document).ready(function() {
         },
         menu: function(event, ui) {
             var text = ui.text;
-            var urlFn = createUrlFn(
-                text === 'People' ? livesearch_people_url : livesearch_url);
-            $('.ui-ls-autocomplete').livesearch('option', 'urlFn', urlFn);
+            var urlFn = text === "All Content"
+                ? createUrlFn(livesearch_url)
+                : createUrlFn(livesearch_url, text);
+            $('.bc-livesearch').livesearch('option', 'urlFn', urlFn);
         },
         validationFn: $.bottlecap.livesearch.prototype.numCharsValidate,
         queryTransformFn: $.bottlecap.livesearch.prototype.globQueryTransform,
         errorFn: $.bottlecap.livesearch.prototype.displayError,
         renderCompletions: renderCompletions
     });
-
-    // The magnifying glass button on the right
-    $(".ui-ls-gobtn").button({
-        text: false,
-        icons: {primary: "ui-icon-search"}
-    });
-
-    // XXX should this be in the livesearch widget itself?
-    // Dynamically set some positioning
-    $('.ui-ls-autocomplete')
-        .height($('.ui-ls-menu').outerHeight())
-        .focus()
-        .position({
-            of: $('.ui-ls-menu'),
-            at: "right top",
-            my: "left top"
-        });
-
 });
-
 
 function renderCompletions(ul, items) {
     var self = this,
@@ -64,13 +43,13 @@ function renderCompletions(ul, items) {
         item.data_value = item.value;
         item.value = self.term;
          if (item.category !== currentCategory) {
-            var li = $('<li class="ui-autocomplete-category"></li>');
+            var li = $('<li class="bc-livesearch-autocomplete-category"></li>');
             li.append(
-                $('<span class="ui-ls-category-text"></span>')
+                $('<span class="bc-livesearch-category-text"></span>')
                     .text(item.category)
             );
             li.append(
-                $('<span class="ui-ls-more"></span>')
+                $('<span class="bc-livesearch-more"></span>')
                     .attr('href', '/search/more')
                     .text('more')
                     .click((function(category) {
@@ -89,38 +68,86 @@ function renderCompletions(ul, items) {
     });
     // Set a class on the first item, to remove a border on
     // the first row
-    ul.find('li:first').addClass('ui-ls-autocomplete-first');
+    ul.find('li:first').addClass('bc-livesearch-autocomplete-first');
+}
+
+var renderDispatchTable = {
+    "profile":   renderPersonEntry,
+    "wikipage":  renderPageEntry,
+    "blogentry": renderPostEntry,
+    "file":      renderFileEntry,
+    "folder":    renderFileEntry
+};
+
+function renderPersonEntry(item) {
+    var entry = $('<a class="bc-livesearch-profile"></a>');
+    entry.append($('<img>')
+                 .attr('src', item.icon));
+    var wrapDiv = $('<div>');
+    var userInfoDiv = $('<div class="user">')
+        .append($('<div>').text(item.label))
+        .append($('<div>').text(item.department));
+    var contactDiv = $('<div class="contact">')
+        .append($('<div>')
+                .append($('<a>')
+                        .attr('href', 'mailto:' + item.email)
+                        .text(item.email)
+                        .click(function() {
+                            window.location = 'mailto:' + item.email;
+                            return false;
+                        })))
+        .append($('<div>').text(item.extension));
+    wrapDiv.append(userInfoDiv).append(contactDiv);
+    entry.append(wrapDiv).append($('<div style="clear: both">'));
+    return entry;
+}
+
+function renderGenericEntry(item) {
+    return $("<a></a>").text(item.label);
+}
+
+function renderPageEntry(item) {
+    var entry = $('<a class="bc-livesearch-page">');
+    entry
+        .append($('<div>')
+                .append($('<span>').text(item.label))
+                .append($('<span class="discreet">').text(
+                    ' - by ' + item.author + ' on ' + item.modified)))
+        .append($('<div>').text(item.community));
+    return entry;
+}
+
+function renderPostEntry(item) {
+    var entry = $('<a class="bc-livesearch-post">');
+    entry
+        .append($('<div>')
+                .append($('<span>').text(item.label))
+                .append($('<span class="discreet">').text(
+                    ' - by ' + item.author + ' on ' + item.created)))
+        .append($('<div>').text(item.community));
+    return entry;
+}
+
+function renderFileEntry(item) {
+    var entry = $('<a class="bc-livesearch-file">');
+    entry
+        .append($('<div>').text(item.label))
+        .append($('<div class="discreet">').text(
+            'by ' + item.author + ' on ' + item.modified));
+    return entry;
 }
 
 function renderItem(ul, item) {
     var li = $('<li>');
-    var entry, div;
-     // Render different items in different ways
-    switch (item.type) {
-        case 'profile': {
-            entry = $('<a class="ui-ls-profile"></a>');
-            entry.append($('<img>').attr('src', item.icon));
-            div = entry.append($('<div>'));
-            div.append(
-                $('<span class="ui-ls-profilelabel"></span>')
-                    .text(item.label)
-            );
-            div.append($('<span>')
-                       .text(item.extension));
-            entry.append($('<div>').text(item.department));
-            break;
-        };
-         default: {
-            entry = $( "<a></a>" ).text( item.label );
-        };
-    };
-    return $( "<li></li>" )
-        .data( "item.autocomplete", item )
-        .append( entry )
-        .appendTo( ul );
+    // Render different items in different ways
+    // dispatch based on the type of the item
+    var type     = item.type,
+        renderFn = renderDispatchTable[type] || renderGenericEntry,
+        entry    = renderFn(item);
+    return $("<li></li>")
+        .data("item.autocomplete", item)
+        .append(entry)
+        .appendTo(ul);
 }
 
-
-
 })(jQuery);
-
