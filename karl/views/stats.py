252ebd0999bd86afe966816a7c30b09d68c467a4
chrisrossi <chris@archimedianco.com>
2011-05-05 14:56:07 +0000

    Stats view now raises NotFound if var/stats/stats.csv is not found.   (LP #777363)

--- karl/views/stats.py
+++ karl/views/stats.py
@@ -3,13 +3,21 @@ import sys
 import os
 
 from repoze.bfg.chameleon_zpt import render_template_to_response
+from repoze.bfg.exceptions import NotFound
 from karl.views.api import TemplateAPI
 
 def stats(context, request):
+    """
+    This appears to no longer be in use.  If we can't confirm someone is using
+    it after some period of time we shoudl probably get rid of it.
+    --rossi 5/5/2011
+    """
     me = sys.argv[0]
     me = os.path.abspath(me)
     sandbox = os.path.dirname(os.path.dirname(me))
     outfile_path = os.path.join(sandbox, 'var/stats/', 'stats.csv')
+    if not os.path.exists(outfile_path):
+        raise NotFound
     f = open(outfile_path, 'r')
     lines = f.readlines()
     resp_out = ""
@@ -19,7 +27,7 @@ def stats(context, request):
         line2 = line
         myarr = line2.split(",")
         line_arrays.append(myarr)
-    
+
     page_title = 'Admin Statistics'
     api = TemplateAPI(context, request, page_title)
 
@@ -29,6 +37,3 @@ def stats(context, request):
         stats = resp_out,
         comms = line_arrays,
         )
-
-    
-    return Response(resp_out)
