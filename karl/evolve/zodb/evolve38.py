a3a8259163532c6c397514c893c416d22173ed1a
chrisrossi <chris@archimedianco.com>
2011-07-27 13:53:09 +0000

    Evolve script to init repo.

---
+++ karl/evolve/zodb/evolve38.py
@@ -0,0 +1,30 @@
+from repoze.folder.interfaces import IFolder
+from repozitory.interfaces import IContainerVersion
+from repozitory.interfaces import IObjectVersion
+from zope.component import queryAdapter
+
+from karl.utils import find_repo
+
+
+def evolve(site):
+    """
+    Initialize repozitory.
+    """
+    repo = find_repo(site)
+    init_repo(repo, site)
+
+
+def init_repo(repo, context):
+    if IFolder.providedBy(context):
+        for child in context.values():
+            init_repo(repo, child)
+
+    version = queryAdapter(context, IObjectVersion)
+    if version is not None:
+        repo.archive(version)
+
+    container = queryAdapter(context, IContainerVersion)
+    if container is not None:
+        repo.archive_container(container, context.creator)
+
+    context._p_deactivate() # try not to run out of memory
