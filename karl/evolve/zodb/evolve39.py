e4840ede72f229117e88100257cbfed6a2117459
chrisrossi <chris@archimedianco.com>
2011-08-02 15:26:23 +0000

    Don't do whole slew of event handlers, just reindex catalog.

--- karl/evolve/zodb/evolve39.py
+++ karl/evolve/zodb/evolve39.py
@@ -1,4 +1,4 @@
-    from repoze.bfg.traversal import model_path
+from repoze.bfg.traversal import model_path
 from zope.component.event import objectEventNotify
 
 from karl.content.models.adapters import extract_text_from_html
@@ -6,6 +6,7 @@ from karl.content.interfaces import IWikiPage
 from karl.events import ObjectModifiedEvent
 from karl.events import ObjectWillBeModifiedEvent
 from karl.models.interfaces import ICatalogSearch
+from karl.utils import find_catalog
 
 
 def evolve(site):
@@ -14,13 +15,12 @@ def evolve(site):
     we start stripping html from the link for purposes of matching the title
     and setting the title for new wiki pages.
     """
+    catalog = find_catalog(site)
     count, docids, resolver = ICatalogSearch(site)(interfaces=[IWikiPage])
     for docid in docids:
         page = resolver(docid)
         cleaned = extract_text_from_html(page.title)
         if page.title != cleaned:
             print "Updating title for %s" % model_path(page)
-            objectEventNotify(ObjectWillBeModifiedEvent(page))
             page.title = cleaned
-            objectEventNotify(ObjectModifiedEvent(page))
-
+            catalog.reindex_doc(page.docid, page)
