249e3b149e4d4b7f73552c193497e7b7c0187202
Chris McDonough <chrism@plope.com>
2011-10-04 04:25:32 -0400

    squash test_tempfolder warnings

--- karl/models/tests/test_tempfolder.py
+++ karl/models/tests/test_tempfolder.py
@@ -2,14 +2,14 @@ import unittest
 
 from pyramid import testing
 
-from zope.testing.cleanup import cleanUp
+import karl.testing
 
 class TestTempFolder(unittest.TestCase):
     def setUp(self):
-        cleanUp()
+        testing.cleanUp()
 
     def tearDown(self):
-        cleanUp()
+        testing.cleanUp()
 
     def _target_class(self):
         from karl.models.tempfolder import TempFolder
@@ -62,7 +62,7 @@ class TestTempFolder(unittest.TestCase):
         self.failIf('two' in obj)
 
     def test___setitem___emits_no_events(self):
-        events = testing.registerEventListener()
+        events = karl.testing.registerEventListener()
         del events[:]
         obj = self._make_one()
         obj['foo'] = testing.DummyModel()
@@ -71,7 +71,7 @@ class TestTempFolder(unittest.TestCase):
     def test___delitem___emits_no_events(self):
         obj = self._make_one()
         obj['foo'] = testing.DummyModel()
-        events = testing.registerEventListener()
+        events = karl.testing.registerEventListener()
         del events[:]
         del obj['foo']
         self.failIf(events)
