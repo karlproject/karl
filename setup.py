8fcf3b53248ac8e68b83e9ceb2a47d14fa454083
Robert Marianski <rob@marianski.com>
2011-12-07 16:44:42 -0500

    release 3.75

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '3.75dev'
+__version__ = '3.75'
 import os
 
 from ez_setup import use_setuptools
