01b77bf50e294e5160b81747757cea0b5e26e90a
Chris Rossi <chris@archimedeanco.com>
2011-12-07 11:48:19 -0500

    More Chameleon2 fixes. Looks like Chameleon2 is less forgiving than its predecessor.

--- karl/application.py
+++ karl/application.py
@@ -1,8 +1,12 @@
 from karl.ux2.layout import LayoutManager
 
 
-def configure_karl(config):
+def configure_karl(config, load_zcml=True):
     config.include('bottlecap')
     config.add_bc_layout({'site': 'karl.ux2:templates/site_layout.pt'})
     config.add_bc_layoutmanager_factory(LayoutManager)
 
+    if load_zcml:
+        config.hook_zca()
+        config.include('pyramid_zcml')
+        config.load_zcml('standalone.zcml')
