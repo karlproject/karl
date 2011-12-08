107f43b9897b8a97d5426f1c704a1f8be3ad2e5b
Paul Everitt <paul@agendaless.com>
2011-12-08 16:27:39 -0500

    Remove unused stuff

--- karl/ux2/helloworld.py
+++ karl/ux2/helloworld.py
@@ -1,10 +1,2 @@
 def helloworld_view(context, request):
     return {"project": "Some Project"}
-
-
-def testpage_view(context, request):
-    return {"project": "Some Project"}
-
-
-def test_component(context, request):
-    return {}
--- karl/ux2/layout.py
+++ karl/ux2/layout.py
@@ -4,13 +4,13 @@ from bottlecap.layout import LayoutManager as BaseLayoutManager
 class LayoutManager(BaseLayoutManager):
 
     @property
-    def my_global_nav_menus(self):
+    def global_nav_menus(self):
         menu_items = [
-            dict(title="Item 1", selected='selected'),
-            dict(title="Item 2", selected=None),
-            dict(title="Item 3", selected=None),
-            dict(title="Item 4", selected=None),
-            dict(title="Item 5", selected=None),
+            dict(title="Intranet", selected=None),
+            dict(title="Communities", selected="selected"),
+            dict(title="People", selected=None),
+            dict(title="Calendar", selected=None),
+            dict(title="Explore", selected=None),
             ]
         return menu_items
 
