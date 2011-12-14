aa8902c3f1068d4d2314f0dce76d703f193233b2
Chris Rossi <chris@archimedeanco.com>
2011-12-14 14:33:47 -0500

    views -> components

---
+++ karl/ux2/components.py
@@ -0,0 +1,18 @@
+
+def global_nav(context, request):
+    return {}
+
+def personal_tools(context, request):
+    return {}
+
+def search(context, request):
+    return {}
+
+def context_tools(context, request):
+    return {}
+
+def actions_menu(context, request):
+    return {}
+
+def column_one(context, request):
+    return {}
