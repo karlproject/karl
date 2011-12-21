01f0f39a669bc553bdf21ceeea1df81bd289bba6
Chris Rossi <chris@archimedeanco.com>
2011-12-21 14:26:20 -0500

    Use default panels for panels that only return empty dicts.

--- karl/ux2/panels.py
+++ karl/ux2/panels.py
@@ -31,21 +31,5 @@ def global_nav(context, request):
     return {'nav_menu': menu_items}
 
 
-def personal_tools(context, request):
-    return {}
-
-
-def search(context, request):
-    return {}
-
-
-def context_tools(context, request):
-    return {}
-
-
 def actions_menu(context, request, actions):
     return {'actions': actions}
-
-
-def column_one(context, request):
-    return {}
