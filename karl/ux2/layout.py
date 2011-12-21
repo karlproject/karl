9a47938bada9e9456b731b23e2b8bb02944d858a
Chris Rossi <chris@archimedeanco.com>
2011-12-21 14:22:15 -0500

    Use panel function for generating nav_menu.

--- karl/ux2/layout.py
+++ karl/ux2/layout.py
@@ -1,6 +1,3 @@
-from pyramid.decorator import reify
-
-from karl.utils import find_intranets
 from karl.views.api import TemplateAPI
 
 
@@ -11,32 +8,3 @@ class Layout(TemplateAPI):
         self.popper_static_url = request.static_url(
             'bottlecap.layouts.popper:static/')
 
-    @reify
-    def global_nav_menus(self):
-        request = self.request
-        site = self.site
-        menu_items = [
-            dict(title="Communities",
-                 url=request.resource_url(site, 'communities'),
-                 selected='selected'),
-            dict(title="People",
-                 url=self.people_url,
-                 selected=None),
-            dict(title="Feeds",
-                 url=request.resource_url(site, 'contentfeeds.html'),
-                 selected=None),
-            ]
-        intranets = find_intranets(site)
-        if self.current_intranet is not None:
-            menu_items.insert(0, dict(title="Intranet",
-                 url=request.resource_url(intranets),
-                 selected=None))
-        if self.should_show_calendar_tab:
-            menu_items.append(dict(title="Calendar",
-                 url=request.resource_url(site, 'offices', 'calendar'),
-                 selected=None))
-        if self.user_is_staff:
-            menu_items.append(dict(title="Tags",
-                 url=request.resource_url(site, 'tagcloud.html'),
-                 selected=None))
-        return menu_items
--- karl/ux2/panels.py
+++ karl/ux2/panels.py
@@ -1,18 +1,51 @@
+from karl.utils import find_intranets
+
 
 def global_nav(context, request):
-    return {}
+    layout = request.layout_manager.layout
+    site = layout.site
+    menu_items = [
+        dict(title="Communities",
+             url=request.resource_url(site, 'communities'),
+             selected='selected'),
+        dict(title="People",
+             url=layout.people_url,
+             selected=None),
+        dict(title="Feeds",
+             url=request.resource_url(site, 'contentfeeds.html'),
+             selected=None),
+        ]
+    intranets = find_intranets(site)
+    if layout.current_intranet is not None:
+        menu_items.insert(0, dict(title="Intranet",
+             url=request.resource_url(intranets),
+             selected=None))
+    if layout.should_show_calendar_tab:
+        menu_items.append(dict(title="Calendar",
+             url=request.resource_url(site, 'offices', 'calendar'),
+             selected=None))
+    if layout.user_is_staff:
+        menu_items.append(dict(title="Tags",
+             url=request.resource_url(site, 'tagcloud.html'),
+             selected=None))
+    return {'nav_menu': menu_items}
+
 
 def personal_tools(context, request):
     return {}
 
+
 def search(context, request):
     return {}
+
 
 def context_tools(context, request):
     return {}
 
+
 def actions_menu(context, request, actions):
     return {'actions': actions}
+
 
 def column_one(context, request):
     return {}
