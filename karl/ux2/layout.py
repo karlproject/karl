d4f7a56882d288fda15aeafa0f6f86f8d047170f
Chris Rossi <chris@archimedeanco.com>
2011-12-21 14:02:46 -0500

    Unholy marriage of layout and template api.

--- karl/ux2/layout.py
+++ karl/ux2/layout.py
@@ -1,43 +1,42 @@
 from pyramid.decorator import reify
-from pyramid.renderers import get_renderer
 
-from bottlecap.layouts.popper.layout import PopperLayout
-
+from karl.utils import find_intranets
 from karl.views.api import TemplateAPI
 
 
-class Layout(PopperLayout):
+class Layout(TemplateAPI):
 
-    @property
-    def global_nav_menus(self):
-        api = TemplateAPI(self.context, self.request)
-        menu_items = [
-            dict(title="Communities",
-                 url="%s/communities" % api.app_url,
-                 selected='selected'),
-            dict(title="People",
-                 url=api.people_url,
-                 selected=None),
-            dict(title="Feeds",
-                 url="%s/contentfeeds.html" % api.app_url,
-                 selected=None),
-            ]
-        if api.current_intranet is not None:
-            menu_items.insert(0, dict(title="Intranet",
-                 url="%s/offices" % api.app_url,
-                 selected=None))
-        if api.should_show_calendar_tab:
-            menu_items.append(dict(title="Calendar",
-                 url="%s/offices/calendar" % api.app_url,
-                 selected=None))
-        if api.user_is_staff:
-            menu_items.append(dict(title="Tags",
-                 url="%s/tagcloud.html" % api.app_url,
-                 selected=None))
-        return menu_items
+    def __init__(self, context, request):
+        super(Layout, self).__init__(context, request)
+        self.popper_static_url = request.static_url(
+            'bottlecap.layouts.popper:static/')
 
     @reify
-    def snippets(self):
-        template = get_renderer('karl:views/templates/snippets.pt')
-        return template.implementation()
-
+    def global_nav_menus(self):
+        request = self.request
+        site = self.site
+        menu_items = [
+            dict(title="Communities",
+                 url=request.resource_url(site, 'communities'),
+                 selected='selected'),
+            dict(title="People",
+                 url=self.people_url,
+                 selected=None),
+            dict(title="Feeds",
+                 url=request.resource_url(site, 'contentfeeds.html'),
+                 selected=None),
+            ]
+        intranets = find_intranets(site)
+        if self.current_intranet is not None:
+            menu_items.insert(0, dict(title="Intranet",
+                 url=request.resource_url(intranets),
+                 selected=None))
+        if self.should_show_calendar_tab:
+            menu_items.append(dict(title="Calendar",
+                 url=request.resource_url(site, 'offices', 'calendar'),
+                 selected=None))
+        if self.user_is_staff:
+            menu_items.append(dict(title="Tags",
+                 url=request.resource_url(site, 'tagcloud.html'),
+                 selected=None))
+        return menu_items
