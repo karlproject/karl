4c1549ff496d656ce2502eff5b874d0050a19e5b
Balazs Ree <ree@greenfinity.hu>
2011-12-22 14:15:31 +0100

    Add Pylons/bottlecap Only the locally needed parts are added: - bc.livesearch - sl/dev-bundles are containing libraries only

---
+++ karl/views/static/bottlecap/sl/min/jquery-ui-1.9m3.js
@@ -0,0 +1,52 @@
+
+/**
+* jQuery UI 1.9m3 resource composition
+* You have to use juicer to produce a minified resource based on this file
+* You cannot use this file as a development resource
+*
+* Core files:
+*     @depends ../dev-bundles/jquery-ui-1.9m3/external/jquery.bgiframe-2.1.2.js        
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.core.js        
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.widget.js       
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.mouse.js          
+*    
+* Behaviour:
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.draggable.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.droppable.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.resizable.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.selectable.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.sortable.js
+*    
+* Widgets:
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.accordion.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.autocomplete.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.button.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.datepicker.js        
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.dialog.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.menu.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.position.js        
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.progressbar.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.slider.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.spinner.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.tabs.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.ui.tooltip.js
+* 
+* Effects:
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.core.js        
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.blind.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.bounce.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.clip.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.core.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.drop.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.explode.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.fade.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.fold.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.highlight.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.pulsate.js        
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.scale.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.shake.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.slide.js
+*     @depends ../dev-bundles/jquery-ui-1.9m3/ui/jquery.effects.transfer.js
+* 
+*/
+
