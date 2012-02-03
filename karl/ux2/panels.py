264705d3f5c2910cc5103e7119e6fc0dd2f7f22a
Balazs Ree <ree@greenfinity.hu>
2012-02-03 18:30:31 +0100

    livesearch: factorize the 'scope' selection possibilities from the search panel. TODO: make it possible to add options from individual views.

--- karl/ux2/panels.py
+++ karl/ux2/panels.py
@@ -1,7 +1,11 @@
 from cgi import escape
+
 from pyramid.security import authenticated_userid
+from pyramid.traversal import resource_path
+
 from karl.utils import find_intranets
 from karl.utils import find_profiles
+from karl.utils import find_community
 from karl.views.utils import get_user_home
 
 
@@ -143,4 +147,27 @@ def skinswitcher(context, request):
         'skinswitcher': dict(value='false', label='LEGACY') \
             if using_ux2 \
             else dict(value='true', label='UX2'),
+        }
+
+
+def search(context, request):
+    scope_options = []
+    scope_options.append(dict(
+        path = '',
+        name = 'all KARL',
+        label = 'all KARL',
+        selected = True,
+        ))
+    # We add a second option, in case, context is inside a community.
+    community = find_community(context)
+    if community:
+        # We are in a community!
+        scope_options.append(dict(
+            path = resource_path(community),
+            name = 'this community',
+            label = community.title,
+        ))
+
+    return {
+        'scope_options': scope_options,
         }
