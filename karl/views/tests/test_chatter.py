cddd3943d9149e144c552a0358849d49b630cf04
Tres Seaver <tseaver@palladion.com>
2012-02-17 15:35:47 -0500

    Add tests for 'add_chatter' view.

--- karl/views/tests/test_chatter.py
+++ karl/views/tests/test_chatter.py
@@ -5,30 +5,8 @@ from pyramid import testing
 from karl import testing as karltesting
 
 
-def _makeChatterbox(recent=()):
 
-    class _Chatterbox(testing.DummyModel):
-        _names = _tag = _community = _added = None
-        def __init__(self, recent):
-            self._recent = recent
-        def addQuip(self, text, creator):
-            self._added = testing.DummyModel(text=text, creator=creator)
-        def recent(self):
-            return self._recent
-        def recentWithNames(self, *names):
-            self._names = names
-            return self._recent
-        def recentWithTag(self, tag):
-            self._tag = tag
-            return self._recent
-        def recentWithCommunity(self, community):
-            self._community = community
-            return self._recent
-
-    return _Chatterbox(recent)
-
-
-class RecentChatterViewTests(unittest.TestCase):
+class Test_recent_chatter(unittest.TestCase):
 
     def setUp(self):
         testing.cleanUp()
@@ -63,3 +41,63 @@ class RecentChatterViewTests(unittest.TestCase):
         request = testing.DummyRequest()
         info = self._callFUT(context, request)
         self.assertEqual(len(list(info['recent'])), 20)
+
+
+class Test_add_chatter(unittest.TestCase):
+
+    def setUp(self):
+        testing.cleanUp()
+
+    def tearDown(self):
+        testing.cleanUp()
+
+    def _callFUT(self, context, request):
+        from karl.views.chatter import add_chatter
+        return add_chatter(context, request)
+
+    def test_it(self):
+        _registerSecurityPolicy('user')
+        context = testing.DummyModel()
+        context['chatter'] = chatterbox = _makeChatterbox()
+        request = testing.DummyRequest(view_name='chatter.html')
+        request.POST['text'] = 'This is a quip.'
+        found = self._callFUT(context, request)
+        self.assertEqual(found.location, 'http://example.com/chatter.html')
+        self.assertEqual(chatterbox._added.text, 'This is a quip.')
+        self.assertEqual(chatterbox._added.creator, 'user')
+
+
+def _makeChatterbox(recent=()):
+
+    class _Chatterbox(testing.DummyModel):
+        _names = _tag = _community = _added = None
+        def __init__(self, recent):
+            self._recent = recent
+        def addQuip(self, text, creator):
+            self._added = testing.DummyModel(text=text, creator=creator)
+        def recent(self):
+            return self._recent
+        def recentWithNames(self, *names):
+            self._names = names
+            return self._recent
+        def recentWithTag(self, tag):
+            self._tag = tag
+            return self._recent
+        def recentWithCommunity(self, community):
+            self._community = community
+            return self._recent
+
+    return _Chatterbox(recent)
+
+
+def _registerSecurityPolicy(userid):
+    karltesting.registerDummySecurityPolicy(userid)
+
+
+class DummySecurityPolicy:
+    """ A standin for both an IAuthentication and IAuthorization policy """
+    def __init__(self, userid=None, groupids=(), permissions=None):
+        self.userid = userid
+
+    def authenticated_userid(self, request):
+        return self.userid
