61a0184d788a33a7eced51d74a67513d8a0ada20
Chris Rossi <chris@archimedeanco.com>
2012-03-06 16:36:17 -0500

    Use context in livesearch (advanced search already does this)

--- karl/utilities/tests/test_groupsearch.py
+++ karl/utilities/tests/test_groupsearch.py
@@ -51,6 +51,29 @@ class TestLiveSearchGroup(unittest.TestCase):
                           'fred', 'group:foo', 'group:bar']
                          )
         self.assertEqual(criteria['allowed']['operator'], 'or')
+        self.assertFalse('path' in criteria)
+
+    def test_makeCriteria_with_path(self):
+        from karl.testing import registerDummySecurityPolicy
+        registerDummySecurityPolicy('fred', ['group:foo', 'group:bar'])
+        request = testing.DummyRequest()
+        site = testing.DummyModel()
+        site['foo'] = context = testing.DummyModel()
+        from zope.interface import Interface
+        thing = self._makeOne(context, request, [Interface], 'foo',
+                              [Interface])
+        criteria = thing._makeCriteria()
+        self.assertEqual(criteria['texts'], 'foo')
+        self.assertEqual(criteria['interfaces']['query'], [Interface])
+        self.assertEqual(criteria['interfaces']['operator'], 'or')
+        self.assertEqual(criteria['containment']['query'], [Interface])
+        self.assertEqual(criteria['containment']['operator'], 'or')
+        self.assertEqual(criteria['allowed']['query'],
+                         ['system.Everyone', 'system.Authenticated',
+                          'fred', 'group:foo', 'group:bar']
+                         )
+        self.assertEqual(criteria['allowed']['operator'], 'or')
+        self.assertEqual(criteria['path'], '/foo')
 
     def test_makeCriteria_no_interfaces(self):
         from repoze.lemonade.interfaces import IContent
@@ -58,7 +81,6 @@ class TestLiveSearchGroup(unittest.TestCase):
         registerDummySecurityPolicy('fred', ['group:foo', 'group:bar'])
         request = testing.DummyRequest()
         context = testing.DummyModel()
-        from zope.interface import Interface
         thing = self._makeOne(context, request, [], 'foo')
         criteria = thing._makeCriteria()
         self.assertEqual(criteria['texts'], 'foo')
