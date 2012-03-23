63192646612228fce54b93b698608ed08872a479
Balazs Ree <ree@greenfinity.hu>
2012-03-23 08:24:57 +0100

    Change the phantom-qunit script to support better logging via a -v parameter (-v prints all console.log-s)

--- karl/phantom-qunit/phantom-qunit.js
+++ karl/phantom-qunit/phantom-qunit.js
@@ -31,22 +31,38 @@ function waitFor(testFx, onReady, timeOutMillis) {
                 }
             }
         }, 100); //< repeat check every 250ms
-};
+}
 
+// parse parameters
+var verbose = false;
+var url;
+if (phantom.args.length >= 1 && phantom.args.length <= 2) {
+    if (phantom.args[0] == '-v' && phantom.args.length == 2) {
+        url = phantom.args[1];
+        verbose = true;
+    } else if (phantom.args[1] == '-v') {
+        verbose = true;
+        url = phantom.args[0];
+    } else if (phantom.args.length == 1) {
+        url = phantom.args[0];
+    }
+}
 
-if (phantom.args.length === 0 || phantom.args.length > 2) {
-    console.log('Usage: run-qunit.js URL');
+if (! url) {
+    console.log('Usage: phantomjs phantom-qunit.js [-v] URL');
     phantom.exit();
 }
 
 var page = new WebPage();
 
 // Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-page.onConsoleMessage = function(msg) {
-    console.log(msg);
-};
+if (verbose) {
+    page.onConsoleMessage = function(msg) {
+        console.log(msg);
+    };
+}
 
-page.open(phantom.args[0], function(status){
+page.open(url, function(status){
     if (status !== "success") {
         console.log("Unable to access network");
         phantom.exit();
@@ -60,15 +76,28 @@ page.open(phantom.args[0], function(status){
                 return false;
             });
         }, function(){
-            var failedNum = page.evaluate(function(){
+            var results = page.evaluate(function(){
                 var el = document.getElementById('qunit-testresult');
-                console.log(el.innerText);
+                console.log(el.innerText + '/n');
                 try {
-                    return el.getElementsByClassName('failed')[0].innerHTML;
+                    return {
+                        total: parseInt(el.getElementsByClassName('total')[0].innerHTML, 10),
+                        passed: parseInt(el.getElementsByClassName('passed')[0].innerHTML, 10),
+                        failed: parseInt(el.getElementsByClassName('failed')[0].innerHTML, 10)
+                    };
                 } catch (e) { }
-                return 10000;
+                return {failed: 11111, total: 0, passed: 0};
             });
-            phantom.exit((parseInt(failedNum, 10) > 0) ? 1 : 0);
+            var prolog;
+            if (results.failed > 0) {
+                prolog = '\x1b[31mFAILED\x1b[37m ';
+            } else {
+                prolog = '\x1b[32mSUCCESS\x1b[37m ';
+            }
+            console.log(prolog + 'Total: ' + results.total + 
+                ', Failed: ' + results.failed +
+                '    of ' + phantom.args[0]);
+            phantom.exit((results.failed > 0) ? 1 : 0);
         });
     }
 });
