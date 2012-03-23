02475967163c233fec421c100464e149ec432d9a
Balazs Ree <ree@greenfinity.hu>
2012-03-23 13:08:44 +0100

    progress phantom script

--- karl/scripts/test_phantom_qunit.py
+++ karl/scripts/test_phantom_qunit.py
@@ -24,12 +24,18 @@ def main(argv=sys.argv):
     verbose = False
     prefix = 'http://127.0.0.1:6543/pg'
 
+    prefix2 = prefix + 'r1332503085/'    # XXX This, of course, cannot work.
+
     tests = [
+        prefix + '/popper-static/popper-plugins/popper-example/tests/test.html',
+        prefix + '/popper-static/popper-plugins/popper-tagbox/tests/test.html',
         prefix + '/popper-static/popper-plugins/popper-pushdown/tests/test.html',
-        #prefix + '/static/karl-plugins/karl-contentfeeds/test.html',
+
+        #prefix2 + '/static/karl-plugins/karl-contentfeeds/test.html',
+        #prefix2 + '/static/tinymce/3.3.9.2/plugins/paste/tests/test.html',
+
         #prefix + '/static/tinymce/3.3.9.2/plugins/imagedrawer/tests/test.html',
         #prefix + '/static/tinymce/3.3.9.2/plugins/kaltura/tests/test.html',
-        #prefix + '/static/tinymce/3.3.9.2/plugins/paste/tests/test.html',
         #prefix + '/static/tinymce/3.3.9.2/plugins/spellchecker/tests/test.html',
         #prefix + '/static/tinymce/3.3.9.2/plugins/wicked/tests/test.html',
         #prefix + '/static/tinymce/3.3.9.2/themes/advanced/skins/karl/tests/test.html',
