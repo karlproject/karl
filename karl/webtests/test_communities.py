978ca79d54c19bd260efff94866863a1aad7e415
Paul Everitt <paul@agendaless.com>
2012-03-23 15:56:41 -0400

    Add another couple of screens to the test, including one that fails due to lp:958597

--- karl/webtests/test_communities.py
+++ karl/webtests/test_communities.py
@@ -1,12 +1,20 @@
 from karl.webtests.base import Base
 
+# Help on the 72 char limit
+dc = '/communities/default'
 
 class TestCommunitiesView(Base):
 
     def test_it(self):
         # After login you'll be at the Default Community home page
-        response = self.login()
-        response = response.click('Communities')
-        response = response.follow() # redirect to Active Communities
-        self.assertTrue('Active' in response)
-        self.assertTrue('Communities' in response)
+        self.login()
+
+        # communities_view, active and all
+        response = self.app.get('/communities/active_communities.html')
+        self.assertTrue('KARL Communities' in response)
+        response = self.app.get('/communities/all_communities.html')
+        self.assertTrue('KARL Communities' in response)
+
+        # wiki_index
+        response = self.app.get(dc + '/wiki/wikitoc.html')
+        self.assertTrue('Wiki Index' in response)
