070f051a5e565ce85511c5ddba4dac0d036375e2
Balazs Ree <ree@greenfinity.hu>
2012-05-14 13:27:28 +0200

    Set a few things right to assure 'deferred' loading and jQuery.ready plays good together.

--- karl/views/static/ux2/js/pushdown.js
+++ karl/views/static/ux2/js/pushdown.js
@@ -1,9 +1,12 @@
-/*globals jQuery:false  */
-(function ($) {
 
-    $(function () {
-        $('body').quickpanel('.pushdown-toggle');
-    });
+/*jslint undef: true, newcap: true, nomen: false, white: true, regexp: true */
+/*jslint plusplus: false, bitwise: true, maxerr: 50, maxlen: 110, indent: 4 */
+/*jslint sub: true */
+/*globals window navigator document console */
+/*globals jQuery:false  */
+
+
+(function ($) {
 
     $.fn.quickpanel = function (selector) {
         return this.each(function () {
@@ -21,12 +24,16 @@
                     $par.addClass('rtl');
                 }
                 $par.toggleClass('open');
-                if(e.currentTarget.id === 'search-toggle') {
+                if (e.currentTarget.id === 'search-toggle') {
                     $par.find('.search-site-box').focus();
                 }
                 return false;
             });
         });
     };
+
+    $(function () {
+        $('body').quickpanel('.pushdown-toggle');
+    });
 
 }(jQuery));
