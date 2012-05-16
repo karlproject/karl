2277e1279dc4fd97b24211208415665de9d1fda3
Balazs Ree <ree@greenfinity.hu>
2012-05-16 08:29:42 +0200

    Hold the jQuery ready event to be triggered, until all our libraries are loaded
    Since we are deferring the loading of our libraries,
    it may happen that the jQuery document ready event is
    fired before some or all of the library code is
    available. This only happens on some browser and sometimes.
    To make sure the ready event does not trigger earlier
    then we want, we hold the event until the last of
    our deferred script has been loaded.
    
    As a remark, this may be the main reason that accounts
    for previously observed IE8 anomalies.

---
+++ karl/views/static/ux2/js/layout-prologue.js
@@ -0,0 +1,15 @@
+
+/*
+ * layout-prologue.js
+ *
+ * This must be an external script and cannot
+ * be inline.
+ *
+ * This script can be concatenated and minified with the others.
+ *
+ */
+
+
+// Release hold on ready event.
+jQuery.holdReady(false);
+
