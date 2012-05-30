1bda662bfe0853aff8da7002d078d05f45aec65e
Balazs Ree <ree@greenfinity.hu>
2012-05-30 18:51:30 +0200

    fix

--- karl/views/static/ux2/js/karl-ux2-legacy.js
+++ karl/views/static/ux2/js/karl-ux2-legacy.js
@@ -3,18 +3,18 @@
 * You have to use juicer to produce a minified resource based on this file
 * You cannot use this file as a development resource
 *
-*     @depends ../../jquery-ui/grid/ui/ui.grid.js',
-*     @depends ../..//jquery-ui/grid/ui/ui.gridmodel.js',
-*     @depends ../../jquery-ui/autobox2/jquery.templating.js',
-*     @depends ../../jquery-ui/autobox2/jquery.ui.autobox.ext.js',
-*     @depends ../../jquery-ui/autobox2/jquery.ui.autobox.js',
-*     @depends ../../karl-plugins/karl-multistatusbox/karl.multistatusbox.js',
-*     @depends ../../karl-plugins/karl-captionedimage/karl.captionedimage.js',
-*     @depends ../../karl-plugins/karl-slider/karl.slider.js',
-*     @depends ../../karl-plugins/karl-buttonset/karl.buttonset.js',
-*     @depends ../../jquery-plugins/jquery.scrollTo.src.js',
-*     @depends ../../jquery-plugins/jquery.tools.js',
-*     @depends ../../karl.js',
+*     @depends ../../jquery-ui/grid/ui/ui.grid.js
+*     @depends ../..//jquery-ui/grid/ui/ui.gridmodel.js
+*     @depends ../../jquery-ui/autobox2/jquery.templating.js
+*     @depends ../../jquery-ui/autobox2/jquery.ui.autobox.ext.js
+*     @depends ../../jquery-ui/autobox2/jquery.ui.autobox.js
+*     @depends ../../karl-plugins/karl-multistatusbox/karl.multistatusbox.js
+*     @depends ../../karl-plugins/karl-captionedimage/karl.captionedimage.js
+*     @depends ../../karl-plugins/karl-slider/karl.slider.js
+*     @depends ../../karl-plugins/karl-buttonset/karl.buttonset.js
+*     @depends ../../jquery-plugins/jquery.scrollTo.src.js
+*     @depends ../../jquery-plugins/jquery.tools.js
+*     @depends ../../karl.js
 *
 */
 
