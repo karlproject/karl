2040d3e4a9ea5542559a1c5daf0054400e0c916a
Balazs Ree <ree@greenfinity.hu>
2012-05-30 18:35:55 +0200

    ux2 minify resources (js only)

---
+++ karl/views/static/ux2/js/karl-ux2-core.js
@@ -0,0 +1,21 @@
+/**
+* KARL ux2 core resource composition
+* You have to use juicer to produce a minified resource based on this file
+* You cannot use this file as a development resource
+*
+*     @depends bootstrap-dropdown.js
+*     @depends bootstrap-modal.js
+*     @depends mustache-0.3.0.js
+*     @depends jquery-ui-1.9m5.min.js
+*     @depends ../plugins/popper-livesearch/bc-core/jquery.cookie.js
+*     @depends ../plugins/popper-livesearch/bc-core/jquery.ajaxmanager-3.0.7.js
+*     @depends ../plugins/popper-livesearch/bc-core/jquery.caret-1.0.2.min.js
+*     @depends ../plugins/popper-livesearch/bc-core/jquery.timeago-0.9.3.js
+*     @depends ../l10n/globalize.js
+*     @depends ../l10n/globalize.cultures.js
+*     @depends ../l10n/globalize.actions.js
+*     @depends ../../jquery-plugins/jquery.timeago.js
+*     @depends ../../jquery-ui/jquery.ui.selectmenu.js
+*
+*/
+
---
+++ karl/views/static/ux2/js/karl-ux2-head.js
@@ -0,0 +1,14 @@
+/**
+* KARL ux2 head resource composition
+* You have to use juicer to produce a minified resource based on this file
+* You cannot use this file as a development resource
+*
+*     @depends jquery-1.7.1.min.js
+*     @depends modernizr-2.0.6.min.js
+*     @depends respond-1.2.0-dev.src.js
+*     @depends ios-orientationchange-fix.js
+*
+*     @ NOT depends ../google/jsapi.js
+*
+*/
+
