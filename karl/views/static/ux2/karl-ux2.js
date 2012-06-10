de2733b5d6690b62cf40e9cb180b299d3957e315
Balazs Ree <ree@greenfinity.hu>
2012-06-10 16:12:15 +0200

    ux2: Wire correctly the edit tags widget on the Edit Files page (since I just fixed the edit tags widget, it is expected that all the forms that have edit tags will start working now.) This fixes LP #983869

--- karl/views/static/ux2/karl-ux2.js
+++ karl/views/static/ux2/karl-ux2.js
@@ -22,14 +22,14 @@
         // need urls
         var appUrl = window.head_data.app_url;
 
-        $('#tagbox').tagbox({
-            prevals: window.head_data.panel_data.tagbox,
-            validateRegexp: "^[a-zA-Z0-9\-\._]+$",
-            searchTagURL: window.head_data.context_url + 'jquery_tag_search',
-            addTagURL: window.head_data.context_url + 'jquery_tag_add',
-            delTagURL: window.head_data.context_url + 'jquery_tag_del',
-            autocompleteURL: appUrl + '/tag_search.json'
-        });
+        //$('#tagbox').tagbox({
+        //    prevals: window.head_data.panel_data.tagbox,
+        //    validateRegexp: "^[a-zA-Z0-9\-\._]+$",
+        //    searchTagURL: window.head_data.context_url + 'jquery_tag_search',
+        //    addTagURL: window.head_data.context_url + 'jquery_tag_add',
+        //    delTagURL: window.head_data.context_url + 'jquery_tag_del',
+        //    autocompleteURL: appUrl + '/tag_search.json'
+        //});
 
 
         function drawChart(elChart, data) {
