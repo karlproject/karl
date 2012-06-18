0bb4753d7f7faed94891434d0c5698cca230b4c7
Balazs Ree <ree@greenfinity.hu>
2012-06-18 15:29:25 +0200

    ux2: Fix the minified resources, after having added more boostrap js.

--- karl/views/static/ux2/js/karl-ux2-core.js
+++ karl/views/static/ux2/js/karl-ux2-core.js
@@ -3,14 +3,15 @@
 * You have to use juicer to produce a minified resource based on this file
 * You cannot use this file as a development resource
 *
+*     @depends jquery-ui-1.9m5.min.js
 *     @depends bootstrap-dropdown.js
 *     @depends bootstrap-modal.js
+*     @depends bootstrap-tooltip.js
+*     @depends bootstrap-popover.js
 *     @depends mustache-0.3.0.js
-*     @depends jquery-ui-1.9m5.min.js
 *     @depends ../plugins/popper-livesearch/bc-core/jquery.cookie.js
 *     @depends ../plugins/popper-livesearch/bc-core/jquery.ajaxmanager-3.0.7.js
 *     @depends ../plugins/popper-livesearch/bc-core/jquery.caret-1.0.2.min.js
-*     @depends ../plugins/popper-livesearch/bc-core/jquery.timeago-0.9.3.js
 *     @depends ../l10n/globalize.js
 *     @depends ../l10n/globalize.cultures.js
 *     @depends ../l10n/globalize.actions.js
