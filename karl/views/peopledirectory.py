e8c7181e0b40517379e887b1b83adcf701b8c811
Balazs Ree <ree@greenfinity.hu>
2012-09-23 17:13:41 +0200

    Make the people grid row just contain 2 lines as in spec (and, not 3, how it was till now.)

--- karl/views/peopledirectory.py
+++ karl/views/peopledirectory.py
@@ -552,7 +552,7 @@ def _slickgrid_info_from_ux2_batch(context, request, batch, columns, columns_jsd
                 'sortDir': sort_dir,
                 },
             # rows accomodate two lines
-            rowHeight=40,
+            rowHeight=39,
             ),
         )
     return result
