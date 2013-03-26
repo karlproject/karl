cacfb0d8ef62dd29cff97400f31710b1a3fc527f
Tres Seaver <tseaver@palladion.com>
2013-03-26 13:11:33 -0400

    Fix attribute error in evolve.

--- karl/evolve/zodb/evolve44.py
+++ karl/evolve/zodb/evolve44.py
@@ -3,11 +3,12 @@ from karl.models.contentfeeds import APPENDSTACK_MAX_LENGTH
 
 def evolve(context):
     events = getattr(context, 'events')
-    if events._max_layers != APPENDSTACK_MAX_LAYERS:
-        print "Update 'events' from %d to %d layers" % (
-             events._max_layers, APPENDSTACK_MAX_LAYERS)
-        events._max_layers = APPENDSTACK_MAX_LAYERS
-    if events._max_length != APPENDSTACK_MAX_LENGTH:
-        print "Update 'events' from %d to %d items per layer" % (
-             events._max_length, APPENDSTACK_MAX_LENGTH)
-        events._max_length = APPENDSTACK_MAX_LENGTH
+    stack = events._stack
+    if stack._max_layers != APPENDSTACK_MAX_LAYERS:
+        print "Update 'events._stack' from %d to %d layers" % (
+             stack._max_layers, APPENDSTACK_MAX_LAYERS)
+        stack._max_layers = APPENDSTACK_MAX_LAYERS
+    if stack._max_length != APPENDSTACK_MAX_LENGTH:
+        print "Update 'events._stack' from %d to %d items per layer" % (
+             stack._max_length, APPENDSTACK_MAX_LENGTH)
+        stack._max_length = APPENDSTACK_MAX_LENGTH
