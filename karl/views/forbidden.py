f603a3b0bb3dd30d3ecd06cc256b7cc4fde4b7fc
Chris Rossi <chris@archimedeanco.com>
2013-06-26 13:25:08 -0400

    That code was probably superfluous.

--- karl/views/forbidden.py
+++ karl/views/forbidden.py
@@ -2,17 +2,9 @@ from pyramid.url import resource_url
 
 from karl.views.api import TemplateAPI
 from karl.utils import find_site
-from pyramid.httpexceptions import HTTPFound
 
 def forbidden(context, request):
     site = find_site(context)
-    if request.referrer and '/login.html' in request.referrer:
-        # this request came from a user submitting the login form
-        request.session['came_from'] = request.url
-        login_url = resource_url(site, request, 'login.html',
-                              query={'reason':'Bad username or password'})
-        return HTTPFound(location=login_url)
-
     request.session['came_from'] = request.url
     api = TemplateAPI(context, request, 'Forbidden')
     request.layout_manager.use_layout('anonymous')
