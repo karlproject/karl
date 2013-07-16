4fe65d0d3d817d0000923eb38faa8caff95c81a6
Chris Rossi <chris@archimedeanco.com>
2013-07-16 10:53:14 -0400

    Apply patch from Gocept.

--- karl/scripts/usersync.py
+++ karl/scripts/usersync.py
@@ -17,7 +17,9 @@ def config_parser(name, subparsers, **helpers):
                         help='Username for BASIC Auth')
     parser.add_argument('--password', '-P', default=None,
                         help='Password for BASIC Auth')
-    parser.add_argument('inst', metavar='instance', help='Karl instnace.')
+    parser.add_argument('--password-file', '-F', default=None,
+                        help='Read password for BASIC Auth from file')
+    parser.add_argument('inst', metavar='instance', help='Karl instance.')
     parser.add_argument('url', help="URL of data source.")
     parser.set_defaults(func=usersync, parser=parser,
                         subsystem='update_feeds', only_one=True)
@@ -27,9 +29,14 @@ def usersync(args):
     root, closer = args.get_root(args.inst)
     set_current_instance(args.inst)
     set_subsystem('usersync')
+    if args.password and args.password_file:
+        raise RuntimeError('cannot set both --password and --password-file')
+    if args.password_file:
+        with open(args.password_file) as f:
+            password = f.read().strip('\n')
+    else:
+        password = args.password
     log.info("Syncing users at %s" % args.url)
     sync = UserSync(root)
-    sync(args.url, args.username, args.password)
+    sync(args.url, args.username, password)
     transaction.commit()
-
-
