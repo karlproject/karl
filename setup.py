d91926abcae611b1de1832ad394fa7a304be2b73
Paul Everitt <paul@agendaless.com>
2013-12-13 06:42:23 -0500

    3.119

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '3.118'
+__version__ = '3.119'
 import os
 
 from setuptools import setup, find_packages
