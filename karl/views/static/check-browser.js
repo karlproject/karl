c9a1917fb149cb0c7085566423623276e2c856d3
Paul Everitt <paul@agendaless.com>
2015-07-02 09:33:12 -0400

    IE11 and 12 use, of course, a different UA string. Fix the detector that shows the bad browser message, then re-run gulp install. LP #1469302.

--- karl/views/static/check-browser.js
+++ karl/views/static/check-browser.js
@@ -1,84 +1,96 @@
-
 /* jshint globalstrict: true */
 /* global document: true */
 
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  // Browser detection, copied from:
-  // https://github.com/jquery/jquery-migrate/blob/master/src/core.js
-  function uaMatch(ua) {
-    ua = ua.toLowerCase();
-    var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
-      /(webkit)[ \/]([\w.]+)/.exec(ua) ||
-      /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
-      /(msie) ([\w.]+)/.exec(ua) ||
-      ua.indexOf('compatible') < 0 &&
-      /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) ||
-      [];
-    return {
-      browser: match[1] || '',
-      version: match[2] || '0'
-    };
-  }
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+        var ua = window.navigator.userAgent;
 
-  // Show the status on the top of the page
-  function showHtml(html) {
-    window.onload = function() {
-      var div = document.createElement("div");
-      div.style.width = '100%';
-      div.style.background = '#eddb33';
-      div.style.color = 'black';
-      div.style.padding = '0.75em';
-      div.style.fontSize = '14px';
-      div.style.zIndex = '99999';
-      // position must be relative, otherwise on IE8 on the login
-      // page the message will not be shown because another
-      // absolutely positioned node covers it.
-      div.style.position = 'relative';
-      div.innerHTML = html;
-      document.body.insertBefore(div, document.body.firstChild);
-    };
-  }
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
 
-  function showStatus(/* arguments */) {
-    // wrap to table for cross browser centering
-    var html = Array.prototype.slice.call(arguments, 0).join(' ');
-    showHtml([
-      '<table style="width: 100%; height: 30px;">',
-        '<tr>',
-          '<td style="text-align: center; vertical-align: middle;">',
-            html,
-          '</td>',
-        '</tr>',
-      '</table>',
-    ].join(' '));
-  }
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
 
-  var meta = document.getElementById('karl-browser-upgrade-url');
-  var browser_upgrade_url = meta ? meta.getAttribute('content') || '' : '';
-  var serviceSwitch = document.location.hash == '#bad-browser';
-  if (browser_upgrade_url || serviceSwitch) {
-    var matched = uaMatch(navigator.userAgent);
-    var isBadBrowser = matched.browser == 'msie' && parseInt(matched.version, 10) < 9;
-    // Show status if we have a bad browser,
-    // or if the #bad-browser service switch hash is added to the url.
-    // (Note: you may need to reload to see the effect.)
-    if (serviceSwitch && browser_upgrade_url === '') {
-      // If this is a testing, let's warn the tester that
-      // not everything is all right.
-      showStatus(
-        'The browser compatibility check is not enabled',
-        '(see browser_upgrade_url configuration option).'
-      );
-    } else if (isBadBrowser || serviceSwitch) {
-      showStatus(
-        'You are using an unsupported version of Internet Explorer.',
-        'Please ',
-        '<a href="' + browser_upgrade_url + '">',
-          'click here to learn more</a>.'
-      );
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+            // IE 12 => return version number
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
     }
-  }
+
+    // Show the status on the top of the page
+    function showHtml(html) {
+        window.onload = function () {
+            var div = document.createElement("div");
+            div.style.width = '100%';
+            div.style.background = '#eddb33';
+            div.style.color = 'black';
+            div.style.padding = '0.75em';
+            div.style.fontSize = '14px';
+            div.style.zIndex = '99999';
+            // position must be relative, otherwise on IE8 on the login
+            // page the message will not be shown because another
+            // absolutely positioned node covers it.
+            div.style.position = 'relative';
+            div.innerHTML = html;
+            document.body.insertBefore(div, document.body.firstChild);
+        };
+    }
+
+    function showStatus(/* arguments */) {
+        // wrap to table for cross browser centering
+        var html = Array.prototype.slice.call(arguments, 0).join(' ');
+        showHtml([
+                     '<table style="width: 100%; height: 30px;">',
+                     '<tr>',
+                     '<td style="text-align: center; vertical-align: middle;">',
+                     html,
+                     '</td>',
+                     '</tr>',
+                     '</table>',
+                 ].join(' '));
+    }
+
+    var meta = document.getElementById('karl-browser-upgrade-url');
+    var browser_upgrade_url = meta ? meta.getAttribute('content') || '' : '';
+    var serviceSwitch = document.location.hash == '#bad-browser';
+    if (browser_upgrade_url || serviceSwitch) {
+        var matched = detectIE();
+        var isBadBrowser = matched && matched < 9;
+        // Show status if we have a bad browser,
+        // or if the #bad-browser service switch hash is added to the url.
+        // (Note: you may need to reload to see the effect.)
+        if (serviceSwitch && browser_upgrade_url === '') {
+            // If this is a testing, let's warn the tester that
+            // not everything is all right.
+            showStatus(
+                'The browser compatibility check is not enabled',
+                '(see browser_upgrade_url configuration option).'
+            );
+        } else if (isBadBrowser || serviceSwitch) {
+            showStatus(
+                'You are using an unsupported version of Internet Explorer.',
+                'Please ',
+                '<a href="' + browser_upgrade_url + '">',
+                'click here to learn more</a>.'
+            );
+        }
+    }
 
 })();
