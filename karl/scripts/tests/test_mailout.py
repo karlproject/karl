32173bf35f680456f0bfc29f15143153818adaa5
Paul Everitt <paul@agendaless.com>
2015-09-12 10:40:35 -0400

    Holy crap, getting mock to work for such a simple case was astonishingly hard. Couldn't get it to reset the iter between tests.

--- karl/scripts/tests/test_mailout.py
+++ karl/scripts/tests/test_mailout.py
@@ -4,52 +4,50 @@ import unittest
 from pyramid import testing
 
 
-class DummyQueueProcessor:
-    def __init__(self, mailer, queue_path):
-        self.mailer = mailer
-        self.queue_path = queue_path
-        self.dummy_output = []
-
+class MockQueueProcessor(object):
     @property
     def maildir(self):
         return iter(range(0, 10))
 
-    def _send_message(self, filename):
-        self.dummy_output.append(filename)
+    _send_message = mock.MagicMock()
 
 
 class Test_mailout(unittest.TestCase):
     def setUp(self):
         testing.cleanUp()
-        self.fake_qp = DummyQueueProcessor(4, 5)
-        self.patcher = mock.patch('repoze.sendmail.queue.QueueProcessor')
-        self.MockClass = self.patcher.start()
-        self.MockClass.return_value = self.fake_qp
-        self.args = DummyArgs()
+        self.args = mock.MagicMock()
         self.env = dict(registry=DummyRegistry())
+        self.mqp = MockQueueProcessor()
 
     def tearDown(self):
-        self.MockClass.return_value = None
-        self.patcher.stop()
         testing.cleanUp()
+        self.mqp._send_message.reset_mock()
 
     def fut(self):
         from karl.scripts.mailout import mailout
         return mailout
 
-    def test_mailout_no_throttle(self):
-        self.fut()(self.args, self.env)
-        self.assertEqual(len(self.fake_qp.dummy_output), 10)
+    def test_no_throttle(self):
+        with mock.patch('repoze.sendmail.queue.QueueProcessor', return_value=self.mqp):
+            self.fut()(self.args, self.env)
+            self.assertEqual(self.mqp._send_message.call_count, 10)
 
-    def test_mailout_throttle(self):
-        # self.env['registry'].mailout_throttle = 2
-        self.fut()(self.args, self.env)
-        # self.assertEqual(len(self.fake_qp.dummy_output), 2)
-        self.assertEqual(self.fake_qp.maildir, 99)
+    def test_no_throttle_second_call(self):
+        with mock.patch('repoze.sendmail.queue.QueueProcessor', return_value=self.mqp):
+            self.fut()(self.args, self.env)
+            self.assertEqual(self.mqp._send_message.call_count, 10)
 
+    def test_over_throttle(self):
+        with mock.patch('repoze.sendmail.queue.QueueProcessor', return_value=self.mqp):
+            self.env['registry'].mailout_throttle = 2
+            self.fut()(self.args, self.env)
+            self.assertEqual(self.mqp._send_message.call_count, 2)
 
-class DummyArgs:
-    server = port = username = password = no_tls = force_tls = None
+    def test_under_throttle(self):
+        with mock.patch('repoze.sendmail.queue.QueueProcessor', return_value=self.mqp):
+            self.env['registry'].mailout_throttle = 12
+            self.fut()(self.args, self.env)
+            self.assertEqual(self.mqp._send_message.call_count, 10)
 
 
 class DummyRegistry:
