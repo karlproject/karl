7d9eaf1c5317f3ab7b534a10a058f7d255832edc
Paul Everitt <pauleveritt@me.com>
2017-03-23 11:32:17 -0700

    4.26

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '4.25.1'
+__version__ = '4.26'
 import os
 
 from setuptools import setup, find_packages
