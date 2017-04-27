6d177ad6e443ed01d1f27ca4de28c37f788546f9
Jim Fulton <jim@jimfulton.info>
2017-04-27 09:23:03 -0400

    added can_view helper and simplified
    Comment out unused helpers (until we use them).
    
    Added can_view helper because it's faster for profile recent items,
    possibly because it adds very little over creator check.
    
    Moved pg/plsql code here so everything is together and to make it
    easier to access from evolution script.
    
    Added __parent__ and __name__ checks to sql generator.  Upper-cased "and".

--- karl/models/newtqbe.py
+++ karl/models/newtqbe.py
@@ -5,31 +5,90 @@ qbe = QBE()
 
 # Punt, it's not used: qbe['containment']
 
-qbe['name'] = match('__name__')
-qbe['title'] = scalar("lower(state->>'title')")
-qbe['titlestartswith'] = scalar("upper(substring(state->>'title', 1, 1))")
+# Comment out unused helpers:
+# qbe['name'] = match('__name__')
+# qbe['title'] = scalar("lower(state->>'title')")
+# qbe['titlestartswith'] = scalar("upper(substring(state->>'title', 1, 1))")
+
+#############################################################################
+# interfaces
+
+interfaces_sql = """
+create or replace function interfaces(class_name_ text) returns text[] as $$
+begin
+  return array(select interface_name from implemented_by i
+               where i.class_name = class_name_);
+end
+$$ language plpgsql immutable cost 99;
+"""
 
 def iface_names(ifaces):
     return [i.__module__ + '.' + i.__name__ for i in ifaces]
+
 qbe['interfaces'] = text_array("interfaces(class_name)", convert=iface_names)
-qbe['texts'] = fulltext("content_text(state)", 'english')
-qbe["path"] = scalar("get_path(state)")
-qbe['allowed'] = text_array('allowed_to_view(state)')
+
+#
+#############################################################################
+
+# qbe['texts'] = fulltext("content_text(state)", 'english')
+# qbe["path"] = scalar("get_path(state)")
+# qbe['allowed'] = text_array('allowed_to_view(state)')
 qbe['creation_date'] = scalar('created')
-qbe['modified_date'] = scalar('modified')
-qbe['content_modified'] = scalar('content_modified')
-qbe['start_date'] = scalar('startDate')
-qbe['end_date'] = scalar('endDate')
-qbe['publication_date'] = scalar('publication_date')
-qbe['mimetype'] = match('mimetype')
+# qbe['modified_date'] = scalar('modified')
+# qbe['content_modified'] = scalar('content_modified')
+# qbe['start_date'] = scalar('startDate')
+# qbe['end_date'] = scalar('endDate')
+# qbe['publication_date'] = scalar('publication_date')
+# qbe['mimetype'] = match('mimetype')
 qbe['creator'] = match('creator')
-qbe['modified_by'] = match('modified_by')
-qbe['email'] = scalar('email')
-qbe['tags'] = text_array("get_tags(state)")
-qbe['lastfirst'] = scalar("get_last_first(class_name, state)")
-qbe['member_name'] = fulltext("get_member_name(class_name, state)",
-                                  'simple')
-qbe['virtual'] = match("calendar_category")
+# qbe['modified_by'] = match('modified_by')
+# qbe['email'] = scalar('email')
+# qbe['tags'] = text_array("get_tags(state)")
+# qbe['lastfirst'] = scalar("get_last_first(class_name, state)")
+# qbe['member_name'] = fulltext("get_member_name(class_name, state)",
+#                                   'simple')
+# qbe['virtual'] = match("calendar_category")
+
+#############################################################################
+# can_view: unindexed security check which is sometimes faster
+
+can_view_sql = """
+create or replace function newt_can_view(state jsonb, principals text[])
+  returns bool
+as $$
+declare
+  acl jsonb;
+  acli jsonb;
+  parent_id bigint;
+  want constant text[] := array['view', '::'];
+begin
+  if state is null then return false; end if;
+  acl := state -> '__acl__';
+  if acl is not null then
+    for i in 0 .. (jsonb_array_length(acl) - 1)
+    loop
+      acli := acl -> i;
+      if acli ->> 1 = any(principals) and acli -> 2 ?| want then
+         return acli ->> 0 = 'Allow';
+      end if;
+    end loop;
+  end if;
+  parent_id := (state -> '__parent__' ->> '::=>')::bigint;
+  if parent_id is null then return false; end if;
+  select newt.state from newt where zoid = parent_id
+  into state;
+  return can_view(state, principals);
+end
+$$ language plpgsql immutable;
+"""
+
+def can_view(cursor, query):
+    return cursor.mogrify("newt_can_view(state, %s)", (query,))
+
+qbe['can_view'] = can_view
+
+#
+#############################################################################
 
 def ob_resolver(ob):
     # satisfy catalog api
@@ -60,7 +119,7 @@ class SQLCatalogSearch(object):
 
         conn = self.context._p_jar
         sql = qbe.sql(conn, dequerify(kw), order_by=order_by)
-        sql = "state ? '__parent__' and state ? '__name__' and " + sql
+        sql = "state ? '__parent__' AND state ? '__name__' AND \n" + sql
 
         if limit is not None:
             if want_count:
@@ -82,6 +141,7 @@ class SQLCatalogSearch(object):
             order_by = ()
 
         sql = qbe.sql(self.context._p_jar, dequerify(kw), order_by=order_by)
+        sql = "state ? '__parent__' AND state ? '__name__' AND \n" + sql
 
         if limit is not None:
             sql += ' limit %d' % limit
